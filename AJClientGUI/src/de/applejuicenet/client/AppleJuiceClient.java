8226b26d51e3dece4182114d845273a6a39bfef8
maj0r <maj0r>
2003-06-10 12:01:50 +0000

    Historie eingefügt.

--- AJClientGUI/src/de/applejuicenet/client/AppleJuiceClient.java
+++ AJClientGUI/src/de/applejuicenet/client/AppleJuiceClient.java
@@ -7,13 +7,21 @@ import de.applejuicenet.client.gui.*;
 import de.applejuicenet.client.gui.controller.*;
 import de.applejuicenet.client.shared.*;
 
-/**
+/*
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/AppleJuiceClient.java,v 1.10 2003/06/10 12:01:50 maj0r Exp $
+ *
  * <p>Title: AppleJuice Client-GUI</p>
  * <p>Description: </p>
  * <p>Copyright: Copyright (c) 2003</p>
  * <p>Company: </p>
  * @author Maj0r
  * @version 1.0
+ *
+ * $Log: AppleJuiceClient.java,v $
+ * Revision 1.10  2003/06/10 12:01:50  maj0r
+ * Historie eingefügt.
+ *
+ *
  */
 
 public class AppleJuiceClient {
