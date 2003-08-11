0898a455f77f6d8148d27b6cbe7ce29bccd8df57
maj0r <maj0r>
2003-08-11 16:52:22 +0000

    DownloadPartList eingefügt.

---
+++ AJClientGUI/src/de/applejuicenet/client/shared/dac/PartListDO.java
@@ -0,0 +1,81 @@
+package de.applejuicenet.client.shared.dac;
+
+/**
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/shared/dac/Attic/PartListDO.java,v 1.1 2003/08/11 16:52:22 maj0r Exp $
+ *
+ * <p>Titel: AppleJuice Client-GUI</p>
+ * <p>Beschreibung: Erstes GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
+ * <p>Copyright: open-source</p>
+ *
+ * @author: Maj0r <AJCoreGUI@maj0r.de>
+ *
+ * $Log: PartListDO.java,v $
+ * Revision 1.1  2003/08/11 16:52:22  maj0r
+ * DownloadPartList eingefügt.
+ *
+ *
+ */
+
+import java.awt.*;
+import java.util.HashSet;
+
+public class PartListDO {
+    public static final Color COLOR_TYPE_OK = Color.BLACK;
+    public static final Color COLOR_TYPE_0 = Color.RED;
+    public static final Color COLOR_TYPE_1 = new Color(25, 25, 250);
+    public static final Color COLOR_TYPE_2 = new Color(50, 50, 250);
+    public static final Color COLOR_TYPE_3 = new Color(75, 75, 250);
+    public static final Color COLOR_TYPE_4 = new Color(100, 100, 250);
+    public static final Color COLOR_TYPE_5 = new Color(125, 125, 250);
+    public static final Color COLOR_TYPE_6 = new Color(150, 150, 250);
+    public static final Color COLOR_TYPE_7 = new Color(175, 175, 250);
+    public static final Color COLOR_TYPE_8 = new Color(200, 200, 250);
+    public static final Color COLOR_TYPE_9 = new Color(225, 225, 250);
+    public static final Color COLOR_TYPE_10 = Color.BLUE;
+
+    private DownloadDO downloadDO;
+    private Long groesse;
+    private HashSet parts = new HashSet();
+
+    public PartListDO(DownloadDO downloadDO){
+        this.downloadDO = downloadDO;
+    }
+
+    public Long getGroesse() {
+        return groesse;
+    }
+
+    public void addPart(Part aPart){
+        parts.add(aPart);
+    }
+
+    public Part[] getParts(){
+        return (Part[])parts.toArray(new Part[parts.size()]);
+    }
+
+    public void setGroesse(Long groesse) {
+        this.groesse = groesse;
+    }
+
+    public DownloadDO getDownloadDO() {
+        return downloadDO;
+    }
+
+    public class Part{
+        private Long fromPosition;
+        private int type;
+
+        public Part(Long fromPosition, int type) {
+            this.fromPosition = fromPosition;
+            this.type = type;
+        }
+
+        public Long getFromPosition() {
+            return fromPosition;
+        }
+
+        public int getType() {
+            return type;
+        }
+   }
+}
