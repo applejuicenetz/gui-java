b3c1b8d797471cf452e2f31f1b6ed2d9081c6d7f
maj0r <maj0r>
2003-08-17 20:55:34 +0000

    Neusortierung der Tabelle nach Änderung entfernt.

--- AJClientGUI/src/de/applejuicenet/client/gui/tables/server/ServerTableModel.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/tables/server/ServerTableModel.java
@@ -11,7 +11,7 @@ import de.applejuicenet.client.gui.shared.TableSorter;
 import de.applejuicenet.client.gui.shared.SortableTableModel;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/tables/server/Attic/ServerTableModel.java,v 1.2 2003/08/02 12:03:38 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/tables/server/Attic/ServerTableModel.java,v 1.3 2003/08/17 20:55:34 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Erstes GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -20,6 +20,9 @@ import de.applejuicenet.client.gui.shared.SortableTableModel;
  * @author: Maj0r <AJCoreGUI@maj0r.de>
  *
  * $Log: ServerTableModel.java,v $
+ * Revision 1.3  2003/08/17 20:55:34  maj0r
+ * Neusortierung der Tabelle nach Änderung entfernt.
+ *
  * Revision 1.2  2003/08/02 12:03:38  maj0r
  * An neue Schnittstelle angepasst.
  *
@@ -156,7 +159,6 @@ public class ServerTableModel
         oldServer.setTryConnect(server.isTryConnect());
       }
     }
-    sortByColumn(0, true);
     this.fireTableDataChanged();
   }
 
