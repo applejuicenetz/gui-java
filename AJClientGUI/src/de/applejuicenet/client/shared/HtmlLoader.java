2519b637524cc3886e146e496090ecd6973f9346
maj0r <maj0r>
2003-08-28 06:55:59 +0000

    Erweiter, so dass nun auch GET/POST ohne Returnwert ausgefuehrt werden koennen.

--- AJClientGUI/src/de/applejuicenet/client/shared/HtmlLoader.java
+++ AJClientGUI/src/de/applejuicenet/client/shared/HtmlLoader.java
@@ -6,27 +6,30 @@ import java.net.*;
 import de.applejuicenet.client.shared.exception.*;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/shared/Attic/HtmlLoader.java,v 1.11 2003/08/15 14:46:30 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/shared/Attic/HtmlLoader.java,v 1.12 2003/08/28 06:55:59 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
- * <p>Beschreibung: Erstes GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
+ * <p>Beschreibung: Erstes GUI fï¿½r den von muhviehstarr entwickelten appleJuice-Core</p>
  * <p>Copyright: open-source</p>
  *
  * @author: Maj0r <AJCoreGUI@maj0r.de>
  *
  * $Log: HtmlLoader.java,v $
+ * Revision 1.12  2003/08/28 06:55:59  maj0r
+ * Erweiter, so dass nun auch GET/POST ohne Returnwert ausgefuehrt werden koennen.
+ *
  * Revision 1.11  2003/08/15 14:46:30  maj0r
  * Refactoring.
  *
  * Revision 1.10  2003/08/09 16:47:42  maj0r
- * Diverse Änderungen.
+ * Diverse ï¿½nderungen.
  *
  * Revision 1.9  2003/07/04 10:35:42  maj0r
  * Lesen des Sockets geht nun wesentlich schneller.
  * Share wird daher wesentlich schneller angezeigt.
  *
  * Revision 1.8  2003/06/10 12:31:03  maj0r
- * Historie eingefügt.
+ * Historie eingefï¿½gt.
  *
  *
  */
@@ -89,7 +92,7 @@ public abstract class HtmlLoader {
       return urlContent.toString();
     }
 
-    public static String getHtmlXMLContent(String host, int method, String command) throws
+    public static String getHtmlXMLContent(String host, int method, String command, boolean withResult) throws
         WebSiteNotFoundException {
         StringBuffer urlContent = new StringBuffer();
         try {
@@ -116,6 +119,9 @@ public abstract class HtmlLoader {
           out.println();
           out.flush();
 
+          if (!withResult){
+              return "ok";
+          }
           BufferedReader in = new BufferedReader(
               new InputStreamReader(
               socket.getInputStream()));
@@ -158,6 +164,11 @@ public abstract class HtmlLoader {
                                              AUTHORIZATION_REQUIRED);
         }
         return urlContent.toString();
+    }
+
+    public static String getHtmlXMLContent(String host, int method, String command) throws
+        WebSiteNotFoundException {
+        return getHtmlXMLContent(host, method, command, true);
     }
   }
 
