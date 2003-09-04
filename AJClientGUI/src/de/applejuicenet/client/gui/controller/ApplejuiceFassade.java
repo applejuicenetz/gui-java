d6632a427a9509f004447a9db3657ea987d7a832
maj0r <maj0r>
2003-09-04 06:34:54 +0000

    Version erhoeht. v 0.22

--- AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
@@ -15,7 +15,7 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.23 2003/09/02 19:29:26 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.24 2003/09/04 06:34:54 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Erstes GUI f�r den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -24,6 +24,10 @@ import org.apache.log4j.Level;
  * @author: Maj0r <AJCoreGUI@maj0r.de>
  *
  * $Log: ApplejuiceFassade.java,v $
+ * Revision 1.24  2003/09/04 06:34:54  maj0r
+ * Version erhoeht.
+ * v 0.22
+ *
  * Revision 1.23  2003/09/02 19:29:26  maj0r
  * Einige Stellen synchronisiert und Nullpointer behoben.
  * Version 0.21 beta.
@@ -162,7 +166,7 @@ import org.apache.log4j.Level;
  */
 
 public class ApplejuiceFassade { //Singleton-Implementierung
-    public static final String GUI_VERSION = "0.21 Beta";
+    public static final String GUI_VERSION = "0.22 Beta";
 
     private HashSet downloadListener;
     private HashSet shareListener;
