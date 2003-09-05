221f48b1d00ade904fb7cb754c3e866174f30b58
maj0r <maj0r>
2003-09-05 09:47:22 +0000

    Version 0.24

--- AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
@@ -15,7 +15,7 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.27 2003/09/05 09:02:26 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.28 2003/09/05 09:47:22 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Erstes GUI f�r den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -24,6 +24,9 @@ import org.apache.log4j.Level;
  * @author: Maj0r <AJCoreGUI@maj0r.de>
  *
  * $Log: ApplejuiceFassade.java,v $
+ * Revision 1.28  2003/09/05 09:47:22  maj0r
+ * Version 0.24
+ *
  * Revision 1.27  2003/09/05 09:02:26  maj0r
  * Threadverwendung verbessert.
  *
@@ -176,7 +179,7 @@ import org.apache.log4j.Level;
  */
 
 public class ApplejuiceFassade { //Singleton-Implementierung
-    public static final String GUI_VERSION = "0.23 Beta";
+    public static final String GUI_VERSION = "0.24 Beta";
 
     private HashSet downloadListener;
     private HashSet shareListener;
