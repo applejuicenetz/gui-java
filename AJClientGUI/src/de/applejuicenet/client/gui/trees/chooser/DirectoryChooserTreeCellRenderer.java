a7bb42fa413a4e18367e1c647a900163ad7bbaf8
maj0r <maj0r>
2003-10-27 12:35:26 +0000

    Bug #1034 fixed (Danke an lov und akkut): Auf den ersten Blick wars ueberfluessig, muss aber weiterhin drin bleiben.

--- AJClientGUI/src/de/applejuicenet/client/gui/trees/chooser/DirectoryChooserTreeCellRenderer.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/trees/chooser/DirectoryChooserTreeCellRenderer.java
@@ -7,15 +7,19 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/trees/chooser/Attic/DirectoryChooserTreeCellRenderer.java,v 1.2 2003/10/21 14:08:45 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/trees/chooser/Attic/DirectoryChooserTreeCellRenderer.java,v 1.3 2003/10/27 12:35:26 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Erstes GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
  * <p>Copyright: open-source</p>
  *
- * @author: Maj0r <AJCoreGUI@maj0r.de>
+ * @author: Maj0r <aj@tkl-soft.de>
  *
  * $Log: DirectoryChooserTreeCellRenderer.java,v $
+ * Revision 1.3  2003/10/27 12:35:26  maj0r
+ * Bug #1034 fixed (Danke an lov und akkut):
+ * Auf den ersten Blick wars ueberfluessig, muss aber weiterhin drin bleiben.
+ *
  * Revision 1.2  2003/10/21 14:08:45  maj0r
  * Mittels PMD Code verschoenert, optimiert.
  *
@@ -31,6 +35,8 @@ public class DirectoryChooserTreeCellRenderer extends DefaultTreeCellRenderer{
                                                   boolean leaf,
                                                   int row, boolean hasFocus) {
 
+      Component c = super.getTreeCellRendererComponent(tree, value,
+                                         sel, expanded, leaf, row, hasFocus);
       Icon icon = ( (Node) value).getConvenientIcon();
       if (icon != null) {
         setIcon(icon);
