246d10bea17d2dfd394a51635594c2909464a873
maj0r <maj0r>
2003-11-24 12:47:08 +0000

    Bug beim autom. Pwdl behoben, der auftrat, wenn nur eine Datei im Download war.

--- AJClientGUI/src/de/applejuicenet/client/gui/powerdownload/StandardAutomaticPwdlPolicy.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/powerdownload/StandardAutomaticPwdlPolicy.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 import de.applejuicenet.client.gui.AppleJuiceDialog;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/powerdownload/Attic/StandardAutomaticPwdlPolicy.java,v 1.4 2003/11/19 19:27:41 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/powerdownload/Attic/StandardAutomaticPwdlPolicy.java,v 1.5 2003/11/24 12:47:08 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Erstes GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -18,6 +18,9 @@ import de.applejuicenet.client.gui.AppleJuiceDialog;
  * @author: Maj0r <aj@tkl-soft.de>
  *
  * $Log: StandardAutomaticPwdlPolicy.java,v $
+ * Revision 1.5  2003/11/24 12:47:08  maj0r
+ * Bug beim autom. Pwdl behoben, der auftrat, wenn nur eine Datei im Download war.
+ *
  * Revision 1.4  2003/11/19 19:27:41  maj0r
  * Handhabung korrigiert.
  *
@@ -92,42 +95,72 @@ public class StandardAutomaticPwdlPolicy extends AutomaticPowerdownloadPolicy {
                         maxProzent[i] = -1;
                     }
                 }
-            }
-            if (maxProzentId[0] == -1 || maxProzentId[1] == -1)
-            {
-                int[] ids = new int[downloads.size()];
-                Iterator it = downloads.values().iterator();
-                int temp = 0;
-                DownloadDO tmpDownloadDO;
-                while (it.hasNext())
-                {
-                    tmpDownloadDO = (DownloadDO) it.next();
-                    if (tmpDownloadDO.getProzentGeladen() > maxProzent[1]
-                            && (tmpDownloadDO.getStatus() == DownloadDO.PAUSIERT
-                            || tmpDownloadDO.getStatus() == DownloadDO.SUCHEN_LADEN))
-                    {
-                        if (tmpDownloadDO.getProzentGeladen() > maxProzent[0]){
-                            maxProzent[1] = maxProzent[0];
-                            maxProzentId[1] = maxProzentId[0];
-                            maxProzent[0] = tmpDownloadDO.getProzentGeladen();
-                            maxProzentId[0] = tmpDownloadDO.getId();
-                        }
-                        else{
-                            maxProzent[1] = tmpDownloadDO.getProzentGeladen();
-                            maxProzentId[1] = tmpDownloadDO.getId();
-                        }
-                    }
-                    ids[temp] = tmpDownloadDO.getId();
-                    temp++;
+                if (maxProzentId[0] == -1 && maxProzentId[1] != -1){
+                    maxProzent[0] = maxProzent[1];
+                    maxProzentId[0] = maxProzentId[1];
+                    maxProzent[1] = -1;
+                    maxProzentId[1] = -1;
                 }
-                if (ids.length!=0){
-                    applejuiceFassade.pauseDownload(ids);
-                    applejuiceFassade.setPowerDownload(ids, 0);
-                    applejuiceFassade.setPowerDownload(new int[] {maxProzentId[
-                        0], maxProzentId[1]}
-                        , pwdlValue);
-                    applejuiceFassade.resumeDownload(new int[] {maxProzentId[0],
-                        maxProzentId[1]});
+            }
+            if (maxProzentId[0] == -1 || (
+                maxProzentId[1] == -1 && downloads.size()!=1))
+            {
+                int downloadSize = downloads.size();
+                int[] ids = new int[downloadSize];
+                Iterator it = downloads.values().iterator();
+                DownloadDO tmpDownloadDO;
+                if (downloadSize==0){
+                    maxProzent[0] = -1;
+                    maxProzentId[0] = -1;
+                    maxProzent[1] = -1;
+                    maxProzentId[1] = -1;
+                }
+                else if (downloadSize==1){
+                    tmpDownloadDO = (DownloadDO) it.next();
+                    maxProzent[0] = tmpDownloadDO.getProzentGeladen();
+                    maxProzentId[0] = tmpDownloadDO.getId();
+                    maxProzent[1] = -1;
+                    maxProzentId[1] = -1;
+                    applejuiceFassade.setPowerDownload(new int[] {
+                        maxProzentId[0]}, pwdlValue);
+                    applejuiceFassade.resumeDownload(new int[] {
+                        maxProzentId[0]});
+                }
+                else{
+                    int temp = 0;
+                    while (it.hasNext()) {
+                        tmpDownloadDO = (DownloadDO) it.next();
+                        if (tmpDownloadDO.getProzentGeladen() > maxProzent[1]
+                            &&
+                            (tmpDownloadDO.getStatus() == DownloadDO.PAUSIERT
+                             ||
+                             tmpDownloadDO.getStatus() == DownloadDO.SUCHEN_LADEN)) {
+                            if (tmpDownloadDO.getProzentGeladen() >
+                                maxProzent[0]) {
+                                maxProzent[1] = maxProzent[0];
+                                maxProzentId[1] = maxProzentId[0];
+                                maxProzent[0] = tmpDownloadDO.getProzentGeladen();
+                                maxProzentId[0] = tmpDownloadDO.getId();
+                            }
+                            else {
+                                maxProzent[1] = tmpDownloadDO.getProzentGeladen();
+                                maxProzentId[1] = tmpDownloadDO.getId();
+                            }
+                        }
+                        ids[temp] = tmpDownloadDO.getId();
+                        temp++;
+                    }
+                    if (ids.length != 0) {
+                        applejuiceFassade.pauseDownload(ids);
+                        applejuiceFassade.setPowerDownload(ids, 0);
+                        applejuiceFassade.setPowerDownload(new int[] {
+                            maxProzentId[
+                            0], maxProzentId[1]}
+                            , pwdlValue);
+                        applejuiceFassade.resumeDownload(new int[] {
+                            maxProzentId[0],
+                            maxProzentId[1]});
+                    }
                 }
             }
         }
