95f2aa3a54402748255f5a4434f12da77a4607ba
maj0r <maj0r>
2003-12-17 11:05:34 +0000

    Version erhoeht.

--- AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
@@ -15,7 +15,7 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.69 2003/12/16 14:51:46 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.70 2003/12/17 11:05:34 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Erstes GUI f�r den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -24,6 +24,9 @@ import org.apache.log4j.Level;
  * @author: Maj0r <aj@tkl-soft.de>
  *
  * $Log: ApplejuiceFassade.java,v $
+ * Revision 1.70  2003/12/17 11:05:34  maj0r
+ * Version erhoeht.
+ *
  * Revision 1.69  2003/12/16 14:51:46  maj0r
  * Suche kann nun GUI-seitig abgebrochen werden.
  *
@@ -308,7 +311,7 @@ import org.apache.log4j.Level;
  */
 
 public class ApplejuiceFassade { //Singleton-Implementierung
-    public static final String GUI_VERSION = "0.47 Beta";
+    public static final String GUI_VERSION = "0.48 Beta";
 
     private HashSet downloadListener;
     private HashSet searchListener;
