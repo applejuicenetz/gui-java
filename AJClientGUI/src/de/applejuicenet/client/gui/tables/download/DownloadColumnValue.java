9d8681f145877612500c54ed7c2f5f51e8e03bbb
maj0r <maj0r>
2004-01-12 09:16:30 +0000

    no message

---
+++ AJClientGUI/src/de/applejuicenet/client/gui/tables/download/DownloadColumnValue.java
@@ -0,0 +1,39 @@
+package de.applejuicenet.client.gui.tables.download;
+
+/**
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/tables/download/Attic/DownloadColumnValue.java,v 1.1 2004/01/12 09:16:30 maj0r Exp $
+ *
+ * <p>Titel: AppleJuice Client-GUI</p>
+ * <p>Beschreibung: Offizielles GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
+ * <p>Copyright: General Public License</p>
+ *
+ * @author: Maj0r <AJCoreGUI@maj0r.de>
+ *
+ * $Log: DownloadColumnValue.java,v $
+ * Revision 1.1  2004/01/12 09:16:30  maj0r
+ * no message
+ *
+ *
+ */
+
+public interface DownloadColumnValue {
+    public String getColumn0();
+
+    public String getColumn1();
+
+    public String getColumn2();
+
+    public String getColumn3();
+
+    public String getColumn4();
+
+    public String getColumn5();
+
+    public String getColumn6();
+
+    public String getColumn7();
+
+    public String getColumn8();
+
+    public String getColumn9();
+}
