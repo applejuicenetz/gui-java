39c1ff38270009fb7032c8e5d524013e12159ca3
maj0r <maj0r>
2004-02-02 15:12:57 +0000

    Version erhoeht.

--- AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
@@ -35,7 +35,7 @@ import java.util.ArrayList;
 import de.applejuicenet.client.gui.AppleJuiceDialog;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.103 2004/01/30 21:34:48 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.104 2004/02/02 15:12:57 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Offizielles GUI fuer den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -275,7 +275,7 @@ import de.applejuicenet.client.gui.AppleJuiceDialog;
  */
 
 public class ApplejuiceFassade { //Singleton-Implementierung
-    public static final String GUI_VERSION = "0.54.7";
+    public static final String GUI_VERSION = "0.55.1";
 
     private HashSet downloadListener;
     private HashSet searchListener;
