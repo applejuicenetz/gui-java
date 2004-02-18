d95f1ff86568477c8479f397eb42607929e386db
maj0r <maj0r>
2004-02-18 21:01:51 +0000

    Version erhoeht.

--- AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/controller/ApplejuiceFassade.java
@@ -29,14 +29,13 @@ import de.applejuicenet.client.shared.NetworkInfo;
 import de.applejuicenet.client.shared.ShareEntry;
 import de.applejuicenet.client.shared.Version;
 import de.applejuicenet.client.shared.dac.DownloadDO;
-import de.applejuicenet.client.shared.dac.DownloadSourceDO;
 import de.applejuicenet.client.shared.dac.PartListDO;
 import de.applejuicenet.client.shared.exception.WebSiteNotFoundException;
 import de.applejuicenet.client.shared.Search;
 import de.applejuicenet.client.gui.controller.xmlholder.PartListXMLHolder;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.114 2004/02/18 17:24:21 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/controller/Attic/ApplejuiceFassade.java,v 1.115 2004/02/18 21:01:51 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Offizielles GUI fuer den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -276,7 +275,7 @@ import de.applejuicenet.client.gui.controller.xmlholder.PartListXMLHolder;
  */
 
 public class ApplejuiceFassade {
-    public static final String GUI_VERSION = "0.55.6";
+    public static final String GUI_VERSION = "0.55.7";
 
     private HashSet downloadListener;
     private HashSet searchListener;
