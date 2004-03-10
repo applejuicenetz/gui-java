d8b98fdadb1207cbe95d35d37ff4ec3de9a861c7
maj0r <maj0r>
2004-03-10 17:04:47 +0000

    Unnuetzes Passwortfeld entfernt.

--- AJClientGUI/src/de/applejuicenet/client/gui/ODConnectionPanel.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/ODConnectionPanel.java
@@ -16,68 +16,21 @@ import javax.swing.JTextField;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import de.applejuicenet.client.gui.controller.ApplejuiceFassade;
 import de.applejuicenet.client.gui.controller.LanguageSelector;
 import de.applejuicenet.client.shared.ConnectionSettings;
 import de.applejuicenet.client.shared.IconManager;
 import de.applejuicenet.client.shared.NumberInputVerifier;
 import de.applejuicenet.client.shared.ZeichenErsetzer;
-import de.applejuicenet.client.gui.controller.ApplejuiceFassade;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/Attic/ODConnectionPanel.java,v 1.13 2004/03/05 15:49:39 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/Attic/ODConnectionPanel.java,v 1.14 2004/03/10 17:04:47 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
- * <p>Beschreibung: Offizielles GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
+ * <p>Beschreibung: Offizielles GUI fuer den von muhviehstarr entwickelten appleJuice-Core</p>
  * <p>Copyright: General Public License</p>
  *
- * @author: Maj0r <AJCoreGUI@maj0r.de>
- *
- * $Log: ODConnectionPanel.java,v $
- * Revision 1.13  2004/03/05 15:49:39  maj0r
- * PMD-Optimierung
- *
- * Revision 1.12  2004/02/21 18:20:30  maj0r
- * LanguageSelector auf SAX umgebaut.
- *
- * Revision 1.11  2004/02/05 23:11:26  maj0r
- * Formatierung angepasst.
- *
- * Revision 1.10  2004/01/25 10:16:42  maj0r
- * Optionenmenue ueberarbeitet.
- *
- * Revision 1.9  2004/01/03 17:29:01  maj0r
- * Dialog bei fehlgeschlagenem Verbindungsversuch überarbeitet.
- *
- * Revision 1.8  2003/12/29 16:04:17  maj0r
- * Header korrigiert.
- *
- * Revision 1.7  2003/12/29 09:39:21  maj0r
- * Alte BugIDs entfernt, da auf neuen Bugtracker auf bugs.applejuicenet.de umgestiegen wurde.
- *
- * Revision 1.6  2003/12/27 19:06:33  maj0r
- * Im Verbindungsfenster geht nun ein einfaches <Enter> (Danke an muhviestarr).
- *
- * Revision 1.5  2003/10/14 19:21:23  maj0r
- * Korrekturen zur Xml-Port-Verwendung.
- *
- * Revision 1.4  2003/10/14 15:43:52  maj0r
- * An pflegbaren Xml-Port angepasst.
- *
- * Revision 1.3  2003/09/09 12:28:15  maj0r
- * Wizard fertiggestellt.
- *
- * Revision 1.2  2003/09/04 10:13:28  maj0r
- * Logger eingebaut.
- *
- * Revision 1.1  2003/08/22 10:55:06  maj0r
- * Klassen umbenannt.
- *
- * Revision 1.4  2003/08/02 12:03:38  maj0r
- * An neue Schnittstelle angepasst.
- *
- * Revision 1.3  2003/06/10 12:31:03  maj0r
- * Historie eingefügt.
- *
+ * @author: Maj0r <aj@tkl-soft.de>
  *
  */
 
@@ -86,12 +39,10 @@ public class ODConnectionPanel
     implements OptionsRegister {
     private boolean dirty = false;
     private JLabel label1;
-    private JLabel label2;
     private JLabel label3;
     private JLabel label4;
     private JTextField host = new JTextField();
     private JTextField port = new JTextField();
-    private JPasswordField passwortAlt = new JPasswordField();
     private JPasswordField passwortNeu = new JPasswordField();
     private ConnectionSettings remote;
     private Logger logger;
@@ -146,8 +97,6 @@ public class ODConnectionPanel
         LanguageSelector languageSelector = LanguageSelector.getInstance();
         label1 = new JLabel(ZeichenErsetzer.korrigiereUmlaute(languageSelector.
             getFirstAttrbuteByTagName(".root.javagui.options.remote.host")));
-        label2 = new JLabel(ZeichenErsetzer.korrigiereUmlaute(languageSelector.
-            getFirstAttrbuteByTagName(".root.javagui.options.remote.passwortAlt")));
         label3 = new JLabel(ZeichenErsetzer.korrigiereUmlaute(languageSelector.
             getFirstAttrbuteByTagName(".root.javagui.options.remote.passwortNeu")));
         menuText = ZeichenErsetzer.korrigiereUmlaute(languageSelector.
@@ -161,12 +110,6 @@ public class ODConnectionPanel
                     dirty = true;
                     remote.setHost(host.getText());
                 }
-            }
-        });
-        passwortAlt.addFocusListener(new FocusAdapter() {
-            public void focusLost(FocusEvent e) {
-                dirty = true;
-                remote.setOldPassword(new String(passwortAlt.getPassword()));
             }
         });
         passwortNeu.addFocusListener(new FocusAdapter() {
@@ -219,10 +162,6 @@ public class ODConnectionPanel
         }
         constraints.gridy = gridy;
         gridy++;
-        panel1.add(label2, constraints);
-
-        constraints.gridy = gridy;
-        gridy++;
         panel1.add(label3, constraints);
 
         constraints.insets.right = 5;
@@ -239,10 +178,6 @@ public class ODConnectionPanel
         }
         constraints.gridy = gridy;
         gridy++;
-        panel1.add(passwortAlt, constraints);
-
-        constraints.gridy = gridy;
-        gridy++;
         panel1.add(passwortNeu, constraints);
 
         constraints.gridy = gridy;
@@ -255,8 +190,6 @@ public class ODConnectionPanel
         if (quickConnectionSettingsDialog != null) {
             label3.setText(ZeichenErsetzer.korrigiereUmlaute(languageSelector.
                 getFirstAttrbuteByTagName(".root.einstform.pwsheet.caption")));
-            passwortAlt.setVisible(false);
-            label2.setVisible(false);
         }
     }
 
@@ -266,10 +199,8 @@ public class ODConnectionPanel
 
     public void enableControls(boolean enable) {
         host.setEnabled(enable);
-        passwortAlt.setEnabled(enable);
         passwortNeu.setEnabled(enable);
         label1.setEnabled(enable);
-        label2.setEnabled(enable);
         label3.setEnabled(enable);
     }
 
