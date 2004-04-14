0b05eee69cb62d6f7b7f866064655e923e4990d5
maj0r <maj0r>
2004-04-14 09:48:34 +0000

    [Maj0r] Bug #273 gefixt Es kam zu einem Fehler, wenn die Partliste nicht den gesamten reservierten Bereich bedeckte und dieser Teil von der Maus ueberwandert wurde.

--- AJClientGUI/src/de/applejuicenet/client/gui/DownloadPartListPanel.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/DownloadPartListPanel.java
@@ -3,111 +3,31 @@ package de.applejuicenet.client.gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import de.applejuicenet.client.gui.controller.ApplejuiceFassade;
+import de.applejuicenet.client.gui.controller.LanguageSelector;
+import de.applejuicenet.client.gui.listener.LanguageListener;
+import de.applejuicenet.client.shared.ZeichenErsetzer;
 import de.applejuicenet.client.shared.dac.DownloadDO;
 import de.applejuicenet.client.shared.dac.DownloadSourceDO;
 import de.applejuicenet.client.shared.dac.PartListDO;
 import de.applejuicenet.client.shared.dac.PartListDO.Part;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseEvent;
-import java.awt.Point;
-import de.applejuicenet.client.gui.controller.LanguageSelector;
-import de.applejuicenet.client.shared.ZeichenErsetzer;
-import de.applejuicenet.client.gui.listener.LanguageListener;
-import de.applejuicenet.client.gui.controller.ApplejuiceFassade;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/Attic/DownloadPartListPanel.java,v 1.28 2004/03/05 15:49:38 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/Attic/DownloadPartListPanel.java,v 1.29 2004/04/14 09:48:34 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Offizielles GUI f\uFFFDr den von muhviehstarr entwickelten appleJuice-Core</p>
  * <p>Copyright: General Public License</p>
  *
- * @author: Maj0r <aj@tkl-soft.de>
- *
- * $Log: DownloadPartListPanel.java,v $
- * Revision 1.28  2004/03/05 15:49:38  maj0r
- * PMD-Optimierung
- *
- * Revision 1.27  2004/03/01 21:20:59  maj0r
- * Natuerlich nur synchronisieren, wenn nicht NULL ...
- *
- * Revision 1.26  2004/03/01 15:46:25  maj0r
- * Bug #254 gefixt
- * Moeglichen NullPointer durch MultiThreading behoben.
- *
- * Revision 1.25  2004/02/26 13:59:41  maj0r
- * Logischen Fehler behoben, wenn es nur ums Neuzeichnen einer vorhandenen Partliste ging.
- *
- * Revision 1.24  2004/02/26 10:38:26  maj0r
- * Partlistverwendung auf Singleton geaendert.
- *
- * Revision 1.23  2004/02/25 11:08:08  maj0r
- * Partliste zeigt nun per MausOver-Effekt den Tooltipp zum ausgewaehlten Partstueck an.
- *
- * Revision 1.22  2004/02/24 08:11:39  maj0r
- * Bug #239 gefixt (Danke an dsp2004)
- * ArrayIndexOutOfBoundsException behoben.
- *
- * Revision 1.21  2004/02/20 14:55:02  maj0r
- * Speicheroptimierungen.
- *
- * Revision 1.20  2004/02/19 13:34:35  maj0r
- * Bild wird freigegeben, wenn die Partliste entfernt wird.
- *
- * Revision 1.19  2004/02/17 14:42:57  maj0r
- * Bug #220 gefixt (Danke an dsp2004)
- * OutOfMemoryError behoben.
- *
- * Revision 1.16  2004/02/12 18:18:32  maj0r
- * Zeichnen des letzten Parts korrigiert.
- *
- * Revision 1.15  2004/02/12 16:36:58  maj0r
- * Anzeige der Teile, die zurzeit uebertragen werden (weiss bis dunkelgelb).
- *
- * Revision 1.14  2004/02/12 10:54:03  maj0r
- * Darstellungsfehler in der Partliste behoben.
- *
- * Revision 1.13  2004/02/09 17:46:11  maj0r
- * Partliste ueberarbeitet.
- *
- * Revision 1.12  2004/02/05 23:11:26  maj0r
- * Formatierung angepasst.
- *
- * Revision 1.11  2003/12/29 16:04:17  maj0r
- * Header korrigiert.
- *
- * Revision 1.10  2003/12/19 14:26:34  maj0r
- * Neuzeichnen korrigiert.
- *
- * Revision 1.9  2003/12/19 13:35:40  maj0r
- * Bug in der Partliste behoben.
- *
- * Revision 1.8  2003/12/16 09:28:04  maj0r
- * NullPointer behoben.
- *
- * Revision 1.7  2003/10/15 09:12:34  maj0r
- * Beim Deaktivieren der Partliste wird diese nun auch zurueck gesetzt,
- *
- * Revision 1.6  2003/10/04 15:30:26  maj0r
- * Userpartliste hinzugefuegt.
- *
- * Revision 1.5  2003/09/07 12:11:59  maj0r
- * Anzeige korrigiert, da in der aktuellen Core auch Verfuegbarkeitswerte > 10 vorkommen koennen.
- *
- * Revision 1.4  2003/09/04 10:14:44  maj0r
- * NullPointer behoben.
- *
- * Revision 1.3  2003/09/04 09:29:18  maj0r
- * Anpassung an die Namenskonvention.
- *
- * Revision 1.2  2003/09/04 09:27:25  maj0r
- * DownloadPartListe fertiggestellt.
- *
+ * @author: Maj0r [maj0r@applejuicenet.de]
  *
  */
 
@@ -414,65 +334,70 @@ public class DownloadPartListPanel
         if (image!=null){
             savedMouseEvent = mouseEvent;
             Point p = mouseEvent.getPoint();
-            int rgb = image.getRGB((int)p.getX(), (int)p.getY());
-            if (rgb == PartListDO.COLOR_TYPE_UEBERPRUEFT.getRGB()) {
-                setToolTipText(ueberprueft);
+            try{
+                int rgb = image.getRGB( (int) p.getX(), (int) p.getY());
+                if (rgb == PartListDO.COLOR_TYPE_UEBERPRUEFT.getRGB()) {
+                    setToolTipText(ueberprueft);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_0.getRGB()) {
+                    setToolTipText(nichtVorhanden);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_OK.getRGB()) {
+                    setToolTipText(vorhanden);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_1.getRGB()) {
+                    setToolTipText("1" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_2.getRGB()) {
+                    setToolTipText("2" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_3.getRGB()) {
+                    setToolTipText("3" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_4.getRGB()) {
+                    setToolTipText("4" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_5.getRGB()) {
+                    setToolTipText("5" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_6.getRGB()) {
+                    setToolTipText("6" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_7.getRGB()) {
+                    setToolTipText("7" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_8.getRGB()) {
+                    setToolTipText("8" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_9.getRGB()) {
+                    setToolTipText("9" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_TYPE_10.getRGB()) {
+                    setToolTipText("10+" + quellen);
+                }
+                else if (rgb == PartListDO.COLOR_READY_10.getRGB()) {
+                    setToolTipText("0-10" + uebertragen);
+                }
+                else if (rgb == PartListDO.COLOR_READY_30.getRGB()) {
+                    setToolTipText("10-30" + uebertragen);
+                }
+                else if (rgb == PartListDO.COLOR_READY_50.getRGB()) {
+                    setToolTipText("30-50" + uebertragen);
+                }
+                else if (rgb == PartListDO.COLOR_READY_70.getRGB()) {
+                    setToolTipText("50-70" + uebertragen);
+                }
+                else if (rgb == PartListDO.COLOR_READY_90.getRGB()) {
+                    setToolTipText("70-90" + uebertragen);
+                }
+                else if (rgb == PartListDO.COLOR_READY_100.getRGB()) {
+                    setToolTipText("90-100" + uebertragen);
+                }
+                else {
+                    setToolTipText(null);
+                }
             }
-            else if (rgb == PartListDO.COLOR_TYPE_0.getRGB()) {
-                setToolTipText(nichtVorhanden);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_OK.getRGB()) {
-                setToolTipText(vorhanden);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_1.getRGB()) {
-                setToolTipText("1" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_2.getRGB()) {
-                setToolTipText("2" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_3.getRGB()) {
-                setToolTipText("3" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_4.getRGB()) {
-                setToolTipText("4" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_5.getRGB()) {
-                setToolTipText("5" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_6.getRGB()) {
-                setToolTipText("6" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_7.getRGB()) {
-                setToolTipText("7" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_8.getRGB()) {
-                setToolTipText("8" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_9.getRGB()) {
-                setToolTipText("9" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_TYPE_10.getRGB()) {
-                setToolTipText("10+" + quellen);
-            }
-            else if (rgb == PartListDO.COLOR_READY_10.getRGB()) {
-                setToolTipText("0-10" + uebertragen);
-            }
-            else if (rgb == PartListDO.COLOR_READY_30.getRGB()) {
-                setToolTipText("10-30" + uebertragen);
-            }
-            else if (rgb == PartListDO.COLOR_READY_50.getRGB()) {
-                setToolTipText("30-50" + uebertragen);
-            }
-            else if (rgb == PartListDO.COLOR_READY_70.getRGB()) {
-                setToolTipText("50-70" + uebertragen);
-            }
-            else if (rgb == PartListDO.COLOR_READY_90.getRGB()) {
-                setToolTipText("70-90" + uebertragen);
-            }
-            else if (rgb == PartListDO.COLOR_READY_100.getRGB()) {
-                setToolTipText("90-100" + uebertragen);
-            }
-            else {
+            catch (ArrayIndexOutOfBoundsException aoobE){
                 setToolTipText(null);
             }
         }
