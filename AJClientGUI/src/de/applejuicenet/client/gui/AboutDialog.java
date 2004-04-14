742320117c125c4673874f9e28862e88a40b4677
maj0r <maj0r>
2004-04-14 15:14:40 +0000

    [Maj0r] Info-Dialog geaendert Credits geaendert. Credits lassen sich nun per Mausklick in den Dialog anhalten bzw. fortsetzen

--- AJClientGUI/src/de/applejuicenet/client/gui/AboutDialog.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/AboutDialog.java
@@ -1,6 +1,7 @@
 package de.applejuicenet.client.gui;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -12,6 +13,8 @@ import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MediaTracker;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.CropImageFilter;
@@ -27,77 +30,22 @@ import de.applejuicenet.client.gui.controller.ApplejuiceFassade;
 import de.applejuicenet.client.gui.controller.LanguageSelector;
 import de.applejuicenet.client.shared.IconManager;
 import de.applejuicenet.client.shared.ZeichenErsetzer;
-import java.util.List;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/Attic/AboutDialog.java,v 1.17 2004/03/05 15:49:38 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/Attic/AboutDialog.java,v 1.18 2004/04/14 15:14:40 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Offizielles GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
  * <p>Copyright: General Public License</p>
  *
- * @author: Maj0r <aj@tkl-soft.de>
- *
- * $Log: AboutDialog.java,v $
- * Revision 1.17  2004/03/05 15:49:38  maj0r
- * PMD-Optimierung
- *
- * Revision 1.16  2004/03/03 15:33:30  maj0r
- * PMD-Optimierung
- *
- * Revision 1.15  2004/02/21 18:20:30  maj0r
- * LanguageSelector auf SAX umgebaut.
- *
- * Revision 1.14  2004/02/05 23:11:26  maj0r
- * Formatierung angepasst.
- *
- * Revision 1.13  2004/01/18 12:21:44  maj0r
- * Credits geaendert.
- *
- * Revision 1.12  2004/01/05 13:55:23  maj0r
- * Credits geaendert.
- *
- * Revision 1.11  2004/01/04 16:36:40  maj0r
- * Credits geaendert.
- *
- * Revision 1.10  2003/12/29 16:04:17  maj0r
- * Header korrigiert.
- *
- * Revision 1.9  2003/12/18 11:27:46  maj0r
- * Creditsanzeige korrigiert.
- *
- * Revision 1.8  2003/11/16 12:34:23  maj0r
- * Themes einngebaut (Danke an LinuxDoc)
- *
- * Revision 1.7  2003/10/21 14:08:45  maj0r
- * Mittels PMD Code verschoenert, optimiert.
- *
- * Revision 1.6  2003/09/06 14:47:40  maj0r
- * Credits angepasst.
- *
- * Revision 1.5  2003/09/05 11:13:27  maj0r
- * Credits geaendert.
- *
- * Revision 1.4  2003/09/05 08:24:40  maj0r
- * Threadverwendung verbessert.
- *
- * Revision 1.3  2003/09/04 22:12:45  maj0r
- * Logger verfeinert.
- * Threadbeendigung korrigiert.
- *
- * Revision 1.2  2003/09/04 13:57:04  maj0r
- * Credits eingebaut.
- *
- * Revision 1.1  2003/08/29 14:24:15  maj0r
- * About-Dialog mit entsprechendem Menuepunkt eingefuehrt.
- *
+ * @author: Maj0r [maj0r@applejuicenet.de]
  *
  */
 
 public class AboutDialog
     extends JDialog {
     private Logger logger;
-    private Thread worker;
+    private WorkerThread worker = null;
     private BackPanel backPanel = new BackPanel();
 
     public AboutDialog(Frame parent, boolean modal) {
@@ -140,7 +88,7 @@ public class AboutDialog
     }
 
     class BackPanel
-        extends JPanel {
+        extends JPanel{
         private Image backgroundImage;
         private Image flagge;
         private JLabel version = new JLabel();
@@ -158,89 +106,23 @@ public class AboutDialog
                     logger.error(ApplejuiceFassade.ERROR_MESSAGE, e);
                 }
             }
-            worker = new Thread() {
-                public void run() {
-                    if (logger.isEnabledFor(Level.DEBUG)) {
-                        logger.debug("About-Workerthread gestartet. " + worker);
-                    }
-                    Image new_img, toDraw;
-                    ImageFilter filter = new ImageFilter();
-                    int creditsHoehe = 60;
-                    int creditsBreite = 135;
-                    int imageX = backgroundImage.getWidth(BackPanel.this) / 2 +
-                        20;
-                    int imageY = backgroundImage.getHeight(BackPanel.this) / 2 -
-                        15;
-                    filter = new CropImageFilter(imageX, imageY, creditsBreite,
-                                                 creditsHoehe);
-                    new_img = createImage(new FilteredImageSource(
-                        backgroundImage.getSource(), filter));
-                    filter = new CropImageFilter(0, 0, creditsBreite,
-                                                 creditsHoehe);
-                    int y = creditsHoehe;
-                    try {
-                        sleep(1000);
-                        Graphics g = BackPanel.this.getGraphics();
-                        g.setColor(Color.BLACK);
-                        CreditsEntry entry;
-                        Graphics toDrawGraphics;
-                        FontMetrics fm;
-                        int strWidth;
-                        while (!isInterrupted()) {
-                            try {
-                                sleep(100);
-                            }
-                            catch (InterruptedException iE) {
-                                interrupt();
-                            }
-                            toDraw = createImage(creditsBreite, creditsHoehe);
-                            toDrawGraphics = toDraw.getGraphics();
-                            toDrawGraphics.drawImage(new_img, 0, 0, BackPanel.this);
-                            y--;
-                            int abstand = -15;
-                            for (int i = 0; i < credits.size(); i++) {
-                                entry = (CreditsEntry) credits.get(i);
-                                if (entry.isUeberschrift()) {
-                                    abstand += 20;
-                                    toDrawGraphics.setFont(new Font("Arial",
-                                        Font.BOLD, 12));
-                                    toDrawGraphics.setColor(Color.BLUE);
-                                }
-                                else {
-                                    abstand += 15;
-                                    toDrawGraphics.setFont(new Font("Arial",
-                                        Font.PLAIN, 12));
-                                    toDrawGraphics.setColor(Color.BLACK);
-                                }
-                                fm = toDrawGraphics.getFontMetrics();
-                                strWidth = fm.stringWidth(entry.getAusgabetext());
-                                toDrawGraphics.drawString(entry.getAusgabetext(),
-                                    (creditsBreite - strWidth) / 2, y + abstand);
-                            }
-                            g.drawImage(toDraw, imageX + 1, imageY - 11,
-                                        BackPanel.this);
-                            if (y == -5 - credits.size() * 15) {
-                                y = creditsHoehe;
-                            }
-                        }
-                    }
-                    catch (Exception e) {
-                        if (logger.isEnabledFor(Level.ERROR)) {
-                            logger.error(ApplejuiceFassade.ERROR_MESSAGE, e);
-                        }
-                    }
-                    if (logger.isEnabledFor(Level.DEBUG)) {
-                        logger.debug("About-Workerthread beendet. " + worker);
+            worker = new WorkerThread(backgroundImage, this, credits);
+            worker.start();
+            addMouseListener(new MouseAdapter(){
+                public void mouseClicked(MouseEvent me){
+                    if (worker!=null){
+                        worker.toggleRunStatus();
                     }
                 }
-            };
-            worker.start();
+            });
         }
 
         private void init() {
             credits.add(new CreditsEntry(true, "Programmierung"));
             credits.add(new CreditsEntry(false, "Maj0r"));
             credits.add(new CreditsEntry(false, "(aj@tkl-soft.de)"));
+            credits.add(new CreditsEntry(false, "loevenwong"));
+            credits.add(new CreditsEntry(false, "(timo@loevenwong.de)"));
             credits.add(new CreditsEntry(true, "Besonderen Dank an"));
             credits.add(new CreditsEntry(false, "muhviehstarr"));
             credits.add(new CreditsEntry(true, "Banner & Bilder"));
@@ -331,6 +213,100 @@ public class AboutDialog
 
         public void setAusgabetext(String ausgabetext) {
             this.ausgabetext = ausgabetext;
+        }
+    }
+
+    private class WorkerThread extends Thread{
+        private Image backgroundImage;
+        private BackPanel backPanel;
+        private List credits;
+        private boolean run = true;
+
+        public WorkerThread(Image backgroundImage, BackPanel backPanel, List credits){
+            this.backgroundImage = backgroundImage;
+            this.backPanel = backPanel;
+            this.credits = credits;
+        }
+
+        public void toggleRunStatus(){
+            run = !run;
+        }
+
+        public void run() {
+            if (logger.isEnabledFor(Level.DEBUG)) {
+                logger.debug("About-Workerthread gestartet. " + worker);
+            }
+            Image new_img, toDraw;
+            ImageFilter filter = new ImageFilter();
+            int creditsHoehe = 60;
+            int creditsBreite = 135;
+            int imageX = backgroundImage.getWidth(backPanel) / 2 +
+                20;
+            int imageY = backgroundImage.getHeight(backPanel) / 2 -
+                15;
+            filter = new CropImageFilter(imageX, imageY, creditsBreite,
+                                         creditsHoehe);
+            new_img = createImage(new FilteredImageSource(
+                backgroundImage.getSource(), filter));
+            filter = new CropImageFilter(0, 0, creditsBreite,
+                                         creditsHoehe);
+            int y = creditsHoehe;
+            try {
+                sleep(1000);
+                Graphics g = backPanel.getGraphics();
+                g.setColor(Color.BLACK);
+                CreditsEntry entry;
+                Graphics toDrawGraphics;
+                FontMetrics fm;
+                int strWidth;
+                while (!isInterrupted()) {
+                    try {
+                        sleep(100);
+                    }
+                    catch (InterruptedException iE) {
+                        interrupt();
+                    }
+                    if (run){
+                        toDraw = createImage(creditsBreite, creditsHoehe);
+                        toDrawGraphics = toDraw.getGraphics();
+                        toDrawGraphics.drawImage(new_img, 0, 0, backPanel);
+                        y--;
+                        int abstand = -15;
+                        for (int i = 0; i < credits.size(); i++) {
+                            entry = (CreditsEntry) credits.get(i);
+                            if (entry.isUeberschrift()) {
+                                abstand += 20;
+                                toDrawGraphics.setFont(new Font("Arial",
+                                    Font.BOLD, 12));
+                                toDrawGraphics.setColor(Color.BLUE);
+                            }
+                            else {
+                                abstand += 15;
+                                toDrawGraphics.setFont(new Font("Arial",
+                                    Font.PLAIN, 12));
+                                toDrawGraphics.setColor(Color.BLACK);
+                            }
+                            fm = toDrawGraphics.getFontMetrics();
+                            strWidth = fm.stringWidth(entry.getAusgabetext());
+                            toDrawGraphics.drawString(entry.getAusgabetext(),
+                                (creditsBreite - strWidth) / 2, y + abstand);
+                        }
+                        g.drawImage(toDraw, imageX + 1, imageY - 11,
+                                    backPanel);
+                        if (y == -5 - credits.size() * 15) {
+                            y = creditsHoehe;
+                        }
+                    }
+                }
+            }
+            catch (Exception e) {
+                if (logger.isEnabledFor(Level.ERROR)) {
+                    logger.error(ApplejuiceFassade.ERROR_MESSAGE, e);
+                }
+            }
+            if (logger.isEnabledFor(Level.DEBUG)) {
+                logger.debug("About-Workerthread beendet. " + worker);
+            }
         }
     }
 }
