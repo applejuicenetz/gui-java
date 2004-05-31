6c7c1f56e4471e4bc9810ba5efce7af9161c18f5
maj0r <maj0r>
2004-05-31 16:22:50 +0000

    Fehler bei leerem Benutzernamen gefixt.

--- AJClientGUI/plugin_src/ircplugin/src/de/applejuicenet/client/gui/plugins/ircplugin/UserListCellRenderer.java
+++ AJClientGUI/plugin_src/ircplugin/src/de/applejuicenet/client/gui/plugins/ircplugin/UserListCellRenderer.java
@@ -1,41 +1,31 @@
 package de.applejuicenet.client.gui.plugins.ircplugin;
 
-import javax.swing.ListCellRenderer;
-import java.awt.Component;
-import javax.swing.JList;
-import javax.swing.JLabel;
 import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/plugin_src/ircplugin/src/de/applejuicenet/client/gui/plugins/ircplugin/UserListCellRenderer.java,v 1.4 2004/05/14 19:48:28 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/plugin_src/ircplugin/src/de/applejuicenet/client/gui/plugins/ircplugin/UserListCellRenderer.java,v 1.5 2004/05/31 16:22:50 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
- * <p>Beschreibung: Erstes GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
+ * <p>Beschreibung: Erstes GUI fuer den von muhviehstarr entwickelten appleJuice-Core</p>
  * <p>Copyright: open-source</p>
  *
- * @author: Maj0r <AJCoreGUI@maj0r.de>
- *
- * $Log: UserListCellRenderer.java,v $
- * Revision 1.4  2004/05/14 19:48:28  maj0r
- * Diverse Bugfixes.
- *
- * Revision 1.3  2004/05/12 12:31:39  maj0r
- * Weitere Arbeiten zum Standardplugin.
- *
- * Revision 1.2  2004/03/03 15:35:45  maj0r
- * PMD-Optimierung
- *
- * Revision 1.1  2003/09/12 06:32:17  maj0r
- * Nur verschoben.
- *
- * Revision 1.2  2003/08/28 15:53:02  maj0r
- * NullPointer behoben und Header eingefuegt.
- *
+ * @author: Maj0r [Maj0r@applejuicenet.de]
  *
  */
 
 
-public class UserListCellRenderer implements ListCellRenderer {
+public class UserListCellRenderer
+    extends JLabel
+    implements ListCellRenderer {
+
+    public UserListCellRenderer(){
+        setOpaque(true);
+    }
+
     public Component getListCellRendererComponent(
             JList list,
             Object value,
@@ -43,36 +33,38 @@ public class UserListCellRenderer implements ListCellRenderer {
             boolean isSelected,
             boolean cellHasFocus) {
 
-        JLabel aLabel = null;
-        aLabel = new JLabel();
-        aLabel.setFont(list.getFont());
-        aLabel.setOpaque(true);
+        setFont(list.getFont());
         if (value != null) {
             String inhalt = (String) value;
-            aLabel.setText(inhalt);
+            setText(inhalt);
             if (isSelected) {
-                aLabel.setBackground(list.getSelectionBackground());
-                aLabel.setForeground(list.getSelectionForeground());
+                setBackground(list.getSelectionBackground());
+                setForeground(list.getSelectionForeground());
             }
             else {
-                aLabel.setForeground(list.getForeground());
-                if (inhalt.substring(0, 1).compareTo("!") == 0) {
-                    aLabel.setBackground(Color.RED);
-                }
-                else if (inhalt.substring(0, 1).compareTo("@") == 0) {
-                    aLabel.setBackground(Color.YELLOW);
-                }
-                else if (inhalt.substring(0, 1).compareTo("%") == 0) {
-                    aLabel.setBackground(Color.GREEN);
-                }
-                else if (inhalt.substring(0, 1).compareTo("+") == 0) {
-                    aLabel.setBackground(Color.LIGHT_GRAY);
+                setForeground(list.getForeground());
+                if (inhalt.length() != 0){
+                    if (inhalt.substring(0, 1).compareTo("!") == 0) {
+                        setBackground(Color.RED);
+                    }
+                    else if (inhalt.substring(0, 1).compareTo("@") == 0) {
+                        setBackground(Color.YELLOW);
+                    }
+                    else if (inhalt.substring(0, 1).compareTo("%") == 0) {
+                        setBackground(Color.GREEN);
+                    }
+                    else if (inhalt.substring(0, 1).compareTo("+") == 0) {
+                        setBackground(Color.LIGHT_GRAY);
+                    }
+                    else {
+                        setBackground(list.getBackground());
+                    }
                 }
                 else {
-                    aLabel.setBackground(list.getBackground());
+                    setBackground(list.getBackground());
                 }
             }
         }
-        return aLabel;
+        return this;
     }
 }
