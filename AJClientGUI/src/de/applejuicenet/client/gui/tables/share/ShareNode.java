9a4ae70e711ecb3e6b93ec4828457db7793f209e
maj0r <maj0r>
2004-07-28 16:35:20 +0000

    Icons in der Sharetable eingebaut.

--- AJClientGUI/src/de/applejuicenet/client/gui/tables/share/ShareNode.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/tables/share/ShareNode.java
@@ -2,101 +2,35 @@ package de.applejuicenet.client.gui.tables.share;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 import de.applejuicenet.client.gui.controller.ApplejuiceFassade;
 import de.applejuicenet.client.gui.tables.Node;
+import de.applejuicenet.client.shared.FileTypeHelper;
 import de.applejuicenet.client.shared.IconManager;
 import de.applejuicenet.client.shared.dac.ShareDO;
-import java.util.Map;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/tables/share/Attic/ShareNode.java,v 1.20 2004/03/03 15:33:31 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/tables/share/Attic/ShareNode.java,v 1.21 2004/07/28 16:35:20 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
- * <p>Beschreibung: Offizielles GUI für den von muhviehstarr entwickelten appleJuice-Core</p>
+ * <p>Beschreibung: Offizielles GUI fuer den von muhviehstarr entwickelten appleJuice-Core</p>
  * <p>Copyright: General Public License</p>
  *
- * @author: Maj0r <AJCoreGUI@maj0r.de>
- *
- * $Log: ShareNode.java,v $
- * Revision 1.20  2004/03/03 15:33:31  maj0r
- * PMD-Optimierung
- *
- * Revision 1.19  2004/02/05 23:11:28  maj0r
- * Formatierung angepasst.
- *
- * Revision 1.18  2004/01/30 16:32:47  maj0r
- * MapSetStringKey ausgebaut.
- *
- * Revision 1.17  2004/01/05 15:30:22  maj0r
- * Bug #43 gefixt (Danke an flabeg)
- * Shareverzeichnis wird bei Prioritaetenaenderung nicht mehr komplett neu geladen, sondern nur aktualsiert.
- *
- * Revision 1.16  2003/12/29 16:04:17  maj0r
- * Header korrigiert.
- *
- * Revision 1.15  2003/12/29 10:54:57  maj0r
- * Bug #4 gefixt (Danke an muhviestarr).
- * Shareanzeige bei Prioritaetenaenderung gefixt.
- *
- * Revision 1.14  2003/12/18 10:48:03  maj0r
- * Bei der Sortierung werden nun Ordner zuerst gelistet.
- *
- * Revision 1.13  2003/12/17 20:35:54  maj0r
- * Bug beim Sortieren der Sharetabelle behoben.
- *
- * Revision 1.12  2003/12/16 18:30:06  maj0r
- * Nun ist es auch wieder plattformunabhaengig.
- *
- * Revision 1.11  2003/12/16 17:05:54  maj0r
- * Sharetabelle auf vielfachen Wunsch komplett überarbeitet.
- *
- * Revision 1.10  2003/10/21 14:08:45  maj0r
- * Mittels PMD Code verschoenert, optimiert.
- *
- * Revision 1.9  2003/09/01 15:50:52  maj0r
- * Wo es moeglich war, DOs auf primitive Datentypen umgebaut.
- *
- * Revision 1.8  2003/08/27 11:19:30  maj0r
- * Prioritaet setzen und aufheben vollstaendig implementiert.
- * Button für 'Share erneuern' eingefuehrt.
- *
- * Revision 1.7  2003/08/26 06:20:10  maj0r
- * Anpassungen an muhs neuen Tree.
- *
- * Revision 1.6  2003/08/25 19:28:52  maj0r
- * Anpassungen an muhs neuen Tree.
- *
- * Revision 1.5  2003/08/25 07:23:25  maj0r
- * Kleine Korrekturen.
- *
- * Revision 1.4  2003/08/24 14:59:59  maj0r
- * Version 0.14
- * Diverse Aenderungen.
- *
- * Revision 1.3  2003/08/15 14:44:48  maj0r
- * Schreibfehler.
- *
- * Revision 1.2  2003/08/14 20:08:42  maj0r
- * Tree fuer Shareauswahl eingefuegt, aber noch nicht fertiggestellt.
- *
- * Revision 1.1  2003/07/02 13:54:34  maj0r
- * JTreeTable komplett überarbeitet.
- *
+ * @author: Maj0r [maj0r@applejuicenet.de]
  *
  */
 
 public class ShareNode
     implements Node {
-    private static ImageIcon leafIcon;
+    private ImageIcon leafIcon = null;
     private static ImageIcon treeIcon;
 
     static {
         IconManager im = IconManager.getInstance();
-        leafIcon = im.getIcon("treeRoot");
         treeIcon = im.getIcon("tree");
     }
 
@@ -163,7 +97,11 @@ public class ShareNode
     }
 
     public Icon getConvenientIcon() {
-        if (path.length() == 0) {
+        if (isLeaf()) {
+        	if (leafIcon == null){
+        		String fileType = FileTypeHelper.calculatePossibleFileType(shareDO.getShortfilename());
+        		leafIcon = IconManager.getInstance().getIcon(fileType);
+        	}
             return leafIcon;
         }
         return treeIcon;
