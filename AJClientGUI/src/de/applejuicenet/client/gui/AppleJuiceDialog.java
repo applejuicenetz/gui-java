ad74dacd94fcee2f31bb87b68a452165d482cc23
maj0r <maj0r>
2005-03-06 20:02:25 +0000

    Version erhoeht.

--- AJClientGUI/src/de/applejuicenet/client/gui/AppleJuiceDialog.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/AppleJuiceDialog.java
@@ -125,7 +125,7 @@ public class AppleJuiceDialog extends TKLFrame implements LanguageListener,
 		DataUpdateListener {
 
 	//CVS-Beispiel 0.60.0-1-CVS
-    public static final String GUI_VERSION = "0.70.0-5-CVS";
+    public static final String GUI_VERSION = "0.70.0-6-CVS";
 	
 	private static Logger logger = Logger.getLogger(AppleJuiceDialog.class);
 	private static Map themes = null;
