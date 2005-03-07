0effadc6b4fb0eb960cbdba2bc659515ac1e07d6
maj0r <maj0r>
2005-03-07 13:41:47 +0000

    Tmp- und Incoming-Baum werden wieder korrekt dargestellt.

--- AJClientGUI/src/de/applejuicenet/client/gui/options/directorytree/DirectoryChooserNode.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/options/directorytree/DirectoryChooserNode.java
@@ -7,13 +7,15 @@ import javax.swing.Icon;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeNode;
 
+import de.applejuicenet.client.AppleJuiceClient;
 import de.applejuicenet.client.fassade.entity.Directory;
+import de.applejuicenet.client.fassade.exception.IllegalArgumentException;
 import de.applejuicenet.client.gui.components.tree.ApplejuiceNode;
 import de.applejuicenet.client.gui.components.treetable.Node;
 import de.applejuicenet.client.shared.IconManager;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/options/directorytree/DirectoryChooserNode.java,v 1.5 2005/01/19 16:22:19 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/options/directorytree/DirectoryChooserNode.java,v 1.6 2005/03/07 13:41:47 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Offizielles GUI fuer den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -27,7 +29,7 @@ public class DirectoryChooserNode
     extends DefaultMutableTreeNode
     implements Node, ApplejuiceNode {
 	private Directory directory;
-    private List children = null;
+    private List<DirectoryChooserNode> children = null;
     private DirectoryChooserNode parent;
 
     public DirectoryChooserNode(DirectoryChooserNode parent,
@@ -39,10 +41,16 @@ public class DirectoryChooserNode
     public DirectoryChooserNode() {
         this.parent = null;
         this.directory = null;
-        children = new ArrayList();
-        //todo
-        
-        //AppleJuiceClient.getAjFassade().getDirectory(null, this);
+        children = new ArrayList<DirectoryChooserNode>();
+        try {
+			List<Directory> directories = AppleJuiceClient.getAjFassade().getDirectories(null);
+            for (Directory curDirectory : directories){
+                addChild(curDirectory);
+            }
+		} catch (IllegalArgumentException e) {
+            // sollte nicht passieren
+			throw new RuntimeException(e);
+		}
     }
 
     public int getChildCount() {
@@ -92,7 +100,7 @@ public class DirectoryChooserNode
 
     public ApplejuiceNode addChild(Directory childDirectory) {
         if (children == null) {
-            children = new ArrayList();
+            children = new ArrayList<DirectoryChooserNode>();
         }
         DirectoryChooserNode childNode = new DirectoryChooserNode(this,
             childDirectory);
@@ -109,7 +117,7 @@ public class DirectoryChooserNode
         for (int i = 0; i < n - 1; i++) {
             k = i;
             for (int j = i + 1; j < n; j++) {
-                if (!((DirectoryChooserNode)children.get(j)).getDirectory().isFileSystem()){
+                if (!children.get(j).getDirectory().isFileSystem()){
                     continue;
                 }
                 if (children.get(j).toString().compareToIgnoreCase(children.get(
@@ -117,7 +125,7 @@ public class DirectoryChooserNode
                     k = j;
                 }
             }
-            Object tmp = children.get(i);
+            DirectoryChooserNode tmp = children.get(i);
             children.set(i, children.get(k));
             children.set(k, tmp);
         }
@@ -125,10 +133,18 @@ public class DirectoryChooserNode
 
     protected Object[] getChildren() {
         if (children == null) {
-            children = new ArrayList();
-            //todo
-            //ApplejuiceFassade.getInstance().getDirectory(directoryDO.getPath(), this);
-            sortChildren();
+            children = new ArrayList<DirectoryChooserNode>();
+            List<Directory> directories;
+			try {
+				directories = AppleJuiceClient.getAjFassade().getDirectories(directory.getPath());
+                for (Directory curDirectory : directories){
+                    addChild(curDirectory);
+                }
+                sortChildren();
+			} catch (IllegalArgumentException e) {
+                // sollte nicht passieren
+                throw new RuntimeException(e);
+			}
         }
         return children.toArray(new DirectoryChooserNode[children.size()]);
     }
