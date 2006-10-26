005e62f47a406b5e2dbf9a45b25aa30fd515fe98
maj0r <maj0r>
2006-10-26 13:50:28 +0000

    ESC eingebaut

--- AJClientGUI/src/de/applejuicenet/client/gui/download/RenameDownloadDialog.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/download/RenameDownloadDialog.java
@@ -6,9 +6,15 @@ import java.awt.FlowLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import java.io.File;
 import java.io.UnsupportedEncodingException;
+
 import java.net.URLEncoder;
+
 import java.util.HashSet;
 
 import javax.swing.JButton;
@@ -22,10 +28,11 @@ import de.applejuicenet.client.fassade.entity.Download;
 import de.applejuicenet.client.fassade.entity.DownloadSource;
 import de.applejuicenet.client.fassade.shared.ZeichenErsetzer;
 import de.applejuicenet.client.gui.controller.LanguageSelector;
+
 import de.tklsoft.gui.controls.TKLComboBox;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/download/RenameDownloadDialog.java,v 1.7 2005/05/06 17:45:10 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/download/RenameDownloadDialog.java,v 1.8 2006/10/26 13:50:28 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Offizielles GUI fuer den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -34,101 +41,139 @@ import de.tklsoft.gui.controls.TKLComboBox;
  * @author: Maj0r <aj@tkl-soft.de>
  *
  */
+public class RenameDownloadDialog extends JDialog
+{
+   private Download    download;
+   private JButton     schliessen        = new JButton();
+   private TKLComboBox possibleNames     = new TKLComboBox();
+   private boolean     somethingSelected = false;
 
-public class RenameDownloadDialog
-    extends JDialog {
-	
-	private Download download;
-    private JButton schliessen = new JButton();
-    private TKLComboBox possibleNames = new TKLComboBox();
-    private boolean somethingSelected = false;
+   public RenameDownloadDialog(JFrame parentDialog, Download selectedDownload)
+   {
+      super(parentDialog, true);
+      download = selectedDownload;
+      init();
+   }
 
-    public RenameDownloadDialog(JFrame parentDialog,
-                                      Download selectedDownload) {
-        super(parentDialog, true);
-        download = selectedDownload;
-        init();
-    }
+   private void init()
+   {
+       KeyListener disposeKeyAdapter = new KeyAdapter()
+       {
+          @Override
+          public void keyPressed(KeyEvent e)
+          {
+             if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+             {
+                setVisible(false);
+             }
+          }
+       };
+      LanguageSelector languageSelector = LanguageSelector.getInstance();
 
-    private void init() {
-        LanguageSelector languageSelector = LanguageSelector.getInstance();
-        setTitle(ZeichenErsetzer.korrigiereUmlaute(languageSelector.
-            getFirstAttrbuteByTagName(".root.mainform.renamefile.caption")));
-        schliessen.setText(ZeichenErsetzer.korrigiereUmlaute(
-            languageSelector.getFirstAttrbuteByTagName(".root.einstform.Button1.caption")));
-        schliessen.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                somethingSelected = true;
-                RenameDownloadDialog.this.dispose();
+      setTitle(ZeichenErsetzer.korrigiereUmlaute(languageSelector.getFirstAttrbuteByTagName(".root.mainform.renamefile.caption")));
+      schliessen.setText(ZeichenErsetzer.korrigiereUmlaute(languageSelector.getFirstAttrbuteByTagName(".root.einstform.Button1.caption")));
+      schliessen.addKeyListener(disposeKeyAdapter);
+      schliessen.addActionListener(new ActionListener()
+         {
+            public void actionPerformed(ActionEvent ae)
+            {
+               somethingSelected = true;
+               RenameDownloadDialog.this.dispose();
             }
-        });
-        JLabel label1 = new JLabel();
-        label1.setText(ZeichenErsetzer.korrigiereUmlaute(languageSelector.
-            getFirstAttrbuteByTagName(".root.javagui.downloadform.neuerdateiname")));
-        HashSet<String> set = new HashSet<String>();
-        set.add(download.getFilename());
-        for (DownloadSource curDownloadSource : download.getSources()) {
-            set.add(curDownloadSource.getFilename());
-        }
-        for (String curname : set){
-            possibleNames.addItem(curname);
-            if (curname.compareTo(download.getFilename()) == 0) {
-                possibleNames.setSelectedItem(curname);
-            }
-        }
-        possibleNames.setEditable(true);
-        possibleNames.getEditor().addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ke) {
-                schliessen.doClick();
-            }
-        });
-        possibleNames.confirmNewValue();
-        JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        panel1.add(label1);
-        panel1.add(possibleNames);
-        getContentPane().setLayout(new BorderLayout());
-        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        southPanel.add(schliessen);
-        getContentPane().add(panel1, BorderLayout.NORTH);
-        getContentPane().add(southPanel, BorderLayout.SOUTH);
-        pack();
-        Dimension appDimension = getSize();
-        Dimension screenSize = Toolkit.getDefaultToolkit().
-            getScreenSize();
-        setLocation( (screenSize.width -
-                      appDimension.width) / 2,
-                    (screenSize.height -
-                     appDimension.height) / 2);
-    }
+         });
 
-    public String getNewName() {
-        if (somethingSelected) {
-            String neuerName = ((String)possibleNames.getSelectedItem()).trim();
-            if (neuerName.indexOf(File.separator) == 0 ||
-                neuerName.indexOf(ApplejuiceFassade.separator) == 0) {
-                neuerName = neuerName.substring(1);
+      JLabel label1 = new JLabel();
+
+      label1.setText(ZeichenErsetzer.korrigiereUmlaute(languageSelector.getFirstAttrbuteByTagName(".root.javagui.downloadform.neuerdateiname")));
+      HashSet<String> set = new HashSet<String>();
+
+      set.add(download.getFilename());
+      for(DownloadSource curDownloadSource : download.getSources())
+      {
+         set.add(curDownloadSource.getFilename());
+      }
+
+      for(String curname : set)
+      {
+         possibleNames.addItem(curname);
+         if(curname.compareTo(download.getFilename()) == 0)
+         {
+            possibleNames.setSelectedItem(curname);
+         }
+      }
+
+      possibleNames.setEditable(true);
+      possibleNames.getEditor().addActionListener(new ActionListener()
+         {
+            public void actionPerformed(ActionEvent ke)
+            {
+               schliessen.doClick();
             }
-            if (neuerName.length()==0){
-                neuerName = null;
+         });
+      possibleNames.getEditor().getEditorComponent().addKeyListener(disposeKeyAdapter);
+      possibleNames.addKeyListener(disposeKeyAdapter);
+      possibleNames.confirmNewValue();
+      JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+      panel1.add(label1);
+      panel1.add(possibleNames);
+      getContentPane().setLayout(new BorderLayout());
+      JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+      southPanel.add(schliessen);
+      getContentPane().add(panel1, BorderLayout.NORTH);
+      getContentPane().add(southPanel, BorderLayout.SOUTH);
+      pack();
+      Dimension appDimension = getSize();
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+      setLocation((screenSize.width - appDimension.width) / 2, (screenSize.height - appDimension.height) / 2);
+      
+      addKeyListener(disposeKeyAdapter);
+   }
+
+   public String getNewName()
+   {
+      if(somethingSelected)
+      {
+         String neuerName = ((String) possibleNames.getSelectedItem()).trim();
+
+         if(neuerName.indexOf(File.separator) == 0 || neuerName.indexOf(ApplejuiceFassade.separator) == 0)
+         {
+            neuerName = neuerName.substring(1);
+         }
+
+         if(neuerName.length() == 0)
+         {
+            neuerName = null;
+         }
+
+         StringBuffer tempLink = new StringBuffer(neuerName);
+
+         for(int i = 0; i < tempLink.length(); i++)
+         {
+            if(tempLink.charAt(i) == ' ')
+            {
+               tempLink.setCharAt(i, '.');
             }
-            StringBuffer tempLink = new StringBuffer(neuerName);
-            for (int i = 0; i < tempLink.length(); i++) {
-                if (tempLink.charAt(i) == ' ') {
-                    tempLink.setCharAt(i, '.');
-                }
-            }
-            try {
-                neuerName = URLEncoder.encode(tempLink.toString(),
-                                              "ISO-8859-1");
-            }
-            catch (UnsupportedEncodingException ex) {
-                ;
-                //gibbet nicht, also nix zu behandeln...
-            }
-            return neuerName;
-        }
-        else {
-            return null;
-        }
-    }
+         }
+
+         try
+         {
+            neuerName = URLEncoder.encode(tempLink.toString(), "ISO-8859-1");
+         }
+         catch(UnsupportedEncodingException ex)
+         {
+            ;
+
+            //gibbet nicht, also nix zu behandeln...
+         }
+
+         return neuerName;
+      }
+      else
+      {
+         return null;
+      }
+   }
 }
--- AJClientGUI/src/de/applejuicenet/client/gui/options/IncomingDirSelectionDialog.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/options/IncomingDirSelectionDialog.java
@@ -6,6 +6,9 @@ import java.awt.FlowLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -14,11 +17,12 @@ import javax.swing.JPanel;
 
 import de.applejuicenet.client.fassade.shared.ZeichenErsetzer;
 import de.applejuicenet.client.gui.controller.LanguageSelector;
+
 import de.tklsoft.gui.controls.TKLButton;
 import de.tklsoft.gui.controls.TKLComboBox;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/options/IncomingDirSelectionDialog.java,v 1.4 2005/05/07 11:28:52 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/options/IncomingDirSelectionDialog.java,v 1.5 2006/10/26 13:50:28 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Offizielles GUI fuer den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -27,77 +31,103 @@ import de.tklsoft.gui.controls.TKLComboBox;
  * @author: Maj0r <aj@tkl-soft.de>
  *
  */
+public class IncomingDirSelectionDialog extends JDialog
+{
+   private String[]    selectionValues;
+   private TKLButton   schliessen        = new TKLButton();
+   private TKLComboBox incomingDirs      = new TKLComboBox();
+   private boolean     somethingSelected = false;
 
-public class IncomingDirSelectionDialog
-    extends JDialog {
+   public IncomingDirSelectionDialog(JFrame parentDialog, String[] selectionValues, String selected)
+   {
+      super(parentDialog, true);
+      this.selectionValues = selectionValues;
+      init(selected);
+   }
 
-    private String[] selectionValues;
-    private TKLButton schliessen = new TKLButton();
-    private TKLComboBox incomingDirs = new TKLComboBox();
-    private boolean somethingSelected = false;
-
-    public IncomingDirSelectionDialog(JFrame parentDialog,
-                                      String[] selectionValues, String selected) {
-        super(parentDialog, true);
-        this.selectionValues = selectionValues;
-        init(selected);
-    }
-
-    private void init(String selected) {
-        LanguageSelector languageSelector = LanguageSelector.getInstance();
-        setTitle(ZeichenErsetzer.korrigiereUmlaute(languageSelector.
-            getFirstAttrbuteByTagName(".root.mainform.changetarget.caption")));
-        schliessen.setText(ZeichenErsetzer.korrigiereUmlaute(
-            languageSelector.getFirstAttrbuteByTagName(".root.einstform.Button1.caption")));
-        schliessen.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                somethingSelected = true;
-                IncomingDirSelectionDialog.this.dispose();
+   private void init(String selected)
+   {
+      KeyListener disposeKeyAdapter = new KeyAdapter()
+      {
+         @Override
+         public void keyPressed(KeyEvent e)
+         {
+            if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+            {
+               setVisible(false);
             }
-        });
-        JLabel label1 = new JLabel();
-        label1.setText(ZeichenErsetzer.korrigiereUmlaute(languageSelector.
-            getFirstAttrbuteByTagName(".root.javagui.downloadform.neuesverzeichnis")));
-        for (int i = 0; i < selectionValues.length; i++) {
-            incomingDirs.addItem(selectionValues[i]);
-            if (selectionValues[i].compareTo("") == 0) {
-                incomingDirs.setSelectedItem(selectionValues[i]);
-            }
-        }
-        incomingDirs.setEditable(true);
-        incomingDirs.getEditor().addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ke) {
-                schliessen.doClick();
-            }
-        });
-        if (selected != null){
-            incomingDirs.setSelectedItem(selected);
-        }
-        incomingDirs.confirmNewValue();
-        JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        panel1.add(label1);
-        panel1.add(incomingDirs);
-        getContentPane().setLayout(new BorderLayout());
-        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        southPanel.add(schliessen);
-        getContentPane().add(panel1, BorderLayout.NORTH);
-        getContentPane().add(southPanel, BorderLayout.SOUTH);
-        pack();
-        Dimension appDimension = getSize();
-        Dimension screenSize = Toolkit.getDefaultToolkit().
-            getScreenSize();
-        setLocation( (screenSize.width -
-                      appDimension.width) / 2,
-                    (screenSize.height -
-                     appDimension.height) / 2);
-    }
+         }
+      };
 
-    public String getSelectedIncomingDir() {
-        if (somethingSelected) {
-            return (String) incomingDirs.getSelectedItem();
-        }
-        else {
-            return null;
-        }
-    }
+      schliessen.addKeyListener(disposeKeyAdapter);
+      incomingDirs.addKeyListener(disposeKeyAdapter);
+      incomingDirs.getEditor().getEditorComponent().addKeyListener(disposeKeyAdapter);
+      addKeyListener(disposeKeyAdapter);
+      LanguageSelector languageSelector = LanguageSelector.getInstance();
+
+      setTitle(ZeichenErsetzer.korrigiereUmlaute(languageSelector.getFirstAttrbuteByTagName(".root.mainform.changetarget.caption")));
+      schliessen.setText(ZeichenErsetzer.korrigiereUmlaute(languageSelector.getFirstAttrbuteByTagName(".root.einstform.Button1.caption")));
+      schliessen.addActionListener(new ActionListener()
+         {
+            public void actionPerformed(ActionEvent ae)
+            {
+               somethingSelected = true;
+               IncomingDirSelectionDialog.this.dispose();
+            }
+         });
+
+      JLabel label1 = new JLabel();
+
+      label1.setText(ZeichenErsetzer.korrigiereUmlaute(languageSelector.getFirstAttrbuteByTagName(".root.javagui.downloadform.neuesverzeichnis")));
+      for(int i = 0; i < selectionValues.length; i++)
+      {
+         incomingDirs.addItem(selectionValues[i]);
+         if(selectionValues[i].compareTo("") == 0)
+         {
+            incomingDirs.setSelectedItem(selectionValues[i]);
+         }
+      }
+
+      incomingDirs.setEditable(true);
+      incomingDirs.getEditor().addActionListener(new ActionListener()
+         {
+            public void actionPerformed(ActionEvent ke)
+            {
+               schliessen.doClick();
+            }
+         });
+      if(selected != null)
+      {
+         incomingDirs.setSelectedItem(selected);
+      }
+
+      incomingDirs.confirmNewValue();
+      JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+      panel1.add(label1);
+      panel1.add(incomingDirs);
+      getContentPane().setLayout(new BorderLayout());
+      JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+      southPanel.add(schliessen);
+      getContentPane().add(panel1, BorderLayout.NORTH);
+      getContentPane().add(southPanel, BorderLayout.SOUTH);
+      pack();
+      Dimension appDimension = getSize();
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+      setLocation((screenSize.width - appDimension.width) / 2, (screenSize.height - appDimension.height) / 2);
+   }
+
+   public String getSelectedIncomingDir()
+   {
+      if(somethingSelected)
+      {
+         return (String) incomingDirs.getSelectedItem();
+      }
+      else
+      {
+         return null;
+      }
+   }
 }
