dacabecf896054b196c6438d948300728052d04f
maj0r <maj0r>
2009-01-04 18:29:17 +0000

    trayicon in eigenes jar ausgelagert, um build fuer java5 zu ermoeglichen

--- AJClientGUI/src/de/applejuicenet/client/gui/start/NachrichtenWorker.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/start/NachrichtenWorker.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2006 TKLSoft.de   All rights reserved.
+ */
+
 package de.applejuicenet.client.gui.start;
 
 import javax.swing.JLabel;
@@ -15,7 +19,7 @@ import de.applejuicenet.client.gui.AppleJuiceDialog;
 import de.applejuicenet.client.gui.controller.ProxyManagerImpl;
 
 /**
- * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/start/NachrichtenWorker.java,v 1.3 2005/01/18 17:35:29 maj0r Exp $
+ * $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/applejuicejava/Repository/AJClientGUI/src/de/applejuicenet/client/gui/start/NachrichtenWorker.java,v 1.4 2009/01/04 18:29:17 maj0r Exp $
  *
  * <p>Titel: AppleJuice Client-GUI</p>
  * <p>Beschreibung: Offizielles GUI fuer den von muhviehstarr entwickelten appleJuice-Core</p>
@@ -24,70 +28,89 @@ import de.applejuicenet.client.gui.controller.ProxyManagerImpl;
  * @author: Maj0r <aj@tkl-soft.de>
  *
  */
+public class NachrichtenWorker extends Thread
+{
+   private final Logger    logger;
+   private StartController startController;
+   private JLabel          version;
+   private JTextPane       nachrichten;
 
-public class NachrichtenWorker extends Thread {
-	private final Logger logger;
-	private StartController startController;
-	private JLabel version;
-	private JTextPane nachrichten;
-	
-	public NachrichtenWorker(StartController startController, JLabel version,
-			JTextPane nachrichten){
-		logger = Logger.getLogger(getClass());
-		this.startController = startController;
-		this.version = version;
-		this.nachrichten = nachrichten;
-	}
-	
-	public void run() {
-		if (logger.isEnabledFor(Level.DEBUG)) {
-			logger.debug("NachrichtenWorkerThread gestartet. " + this);
-		}
-		try {
-			String coreVersion = AppleJuiceClient.getAjFassade()
-					.getCoreVersion().getVersion();
-			version.setText("<html>GUI: " + AppleJuiceDialog.GUI_VERSION  + "/" + 
-					ApplejuiceFassade.FASSADE_VERSION +
-					"<br>Core: " + coreVersion + "</html>");
-			String nachricht = "verwendeter Core: " + coreVersion;
-			if (logger.isEnabledFor(Level.INFO)) {
-				logger.info(nachricht);
-			}
-			ProxySettings proxy = ProxyManagerImpl.getInstance().getProxySettings();
-			String htmlText = WebsiteContentLoader.getWebsiteContent(proxy, 
-					"http://www.applejuicenet.org", 80,
-					"/inprog/news.php?version="
-							+ AppleJuiceClient.getAjFassade()
-									.getCoreVersion().getVersion());
+   public NachrichtenWorker(StartController startController, JLabel version, JTextPane nachrichten)
+   {
+      logger               = Logger.getLogger(getClass());
+      this.startController = startController;
+      this.version         = version;
+      this.nachrichten     = nachrichten;
+   }
 
-			int pos = htmlText.toLowerCase().indexOf("<html>");
-			if (pos != -1) {
-				htmlText = htmlText.substring(pos);
-			} else {
-				htmlText = "<html>" + htmlText + "</html>";
-			}
-			StringBuffer buffer = new StringBuffer(htmlText);
-			int index;
-			while ((index = buffer.indexOf(". ")) != -1) {
-				buffer.replace(index, index + 1, ".<br>");
-			}
-			htmlText = buffer.toString();
-			final String htmlContent = htmlText;
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					nachrichten.setContentType("text/html");
-					nachrichten.setText(htmlContent);
-					nachrichten.setFont(version.getFont());
-				}
-			});
-		} catch (Exception e) {
-			if (logger.isEnabledFor(Level.INFO)) {
-				logger.info("Versionsabhaengige Nachrichten konnten nicht geladen werden. Server down?");
-			}
-		}
-		if (logger.isEnabledFor(Level.DEBUG)) {
-			logger.debug("NachrichtenWorkerThread beendet. " + this);
-		}
-	}
+   public void run()
+   {
+      if(logger.isEnabledFor(Level.DEBUG))
+      {
+         logger.debug("NachrichtenWorkerThread gestartet. " + this);
+      }
 
+      try
+      {
+         final String coreVersion = AppleJuiceClient.getAjFassade().getCoreVersion().getVersion();
+         String       nachricht   = "verwendeter Core: " + coreVersion;
+
+         if(logger.isEnabledFor(Level.INFO))
+         {
+            logger.info(nachricht);
+         }
+
+         ProxySettings proxy    = ProxyManagerImpl.getInstance().getProxySettings();
+         String        htmlText = WebsiteContentLoader.getWebsiteContent(proxy, "http://www.applejuicenet.org", 80,
+                                                                         "/inprog/news.php?version=" +
+                                                                         AppleJuiceClient.getAjFassade().getCoreVersion()
+                                                                         .getVersion());
+
+         int           pos      = htmlText.toLowerCase().indexOf("<html>");
+
+         if(pos != -1)
+         {
+            htmlText = htmlText.substring(pos);
+         }
+         else
+         {
+            htmlText = "<html>" + htmlText + "</html>";
+         }
+
+         StringBuffer buffer = new StringBuffer(htmlText);
+         int          index;
+
+         while((index = buffer.indexOf(". ")) != -1)
+         {
+            buffer.replace(index, index + 1, ".<br>");
+         }
+
+         htmlText            = buffer.toString();
+         final String htmlContent = htmlText;
+
+         SwingUtilities.invokeLater(new Runnable()
+            {
+               public void run()
+               {
+                  version.setText("<html>GUI: " + AppleJuiceDialog.GUI_VERSION + "/" + ApplejuiceFassade.FASSADE_VERSION +
+                                  "<br>Core: " + coreVersion + "</html>");
+                  nachrichten.setContentType("text/html");
+                  nachrichten.setText(htmlContent);
+                  nachrichten.setFont(version.getFont());
+               }
+            });
+      }
+      catch(Exception e)
+      {
+         if(logger.isEnabledFor(Level.INFO))
+         {
+            logger.info("Versionsabhaengige Nachrichten konnten nicht geladen werden. Server down?");
+         }
+      }
+
+      if(logger.isEnabledFor(Level.DEBUG))
+      {
+         logger.debug("NachrichtenWorkerThread beendet. " + this);
+      }
+   }
 }
