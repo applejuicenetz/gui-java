ad86ba021769ac6480242bb6133007975e2c3143
maj0r <maj0r>
2009-01-05 12:56:27 +0000

    Progressbar korrigiert

--- AJClientGUI/src/de/applejuicenet/client/shared/util/DownloadCalculator.java
+++ AJClientGUI/src/de/applejuicenet/client/shared/util/DownloadCalculator.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2006 TKLSoft.de   All rights reserved.
+ */
+
 package de.applejuicenet.client.shared.util;
 
 import org.apache.log4j.Logger;
@@ -7,58 +11,79 @@ import de.applejuicenet.client.fassade.entity.Download;
 import de.applejuicenet.client.fassade.entity.DownloadSource;
 import de.applejuicenet.client.gui.download.table.DownloadModel;
 
-public abstract class DownloadCalculator {
-	private static Logger logger = Logger.getLogger(DownloadCalculator.class); 
+public abstract class DownloadCalculator
+{
+   private static Logger logger = Logger.getLogger(DownloadCalculator.class);
 
-	public static String getStatusAsString(Download download) {
-        try {
-            switch (download.getStatus()) {
-                case Download.PAUSIERT:
-                    return DownloadModel.pausiert;
-                case Download.ABBRECHEN:
-                    return DownloadModel.abbrechen;
-                case Download.ABGEGROCHEN:
-                    return DownloadModel.abgebrochen;
-                case Download.FERTIG:
-                    return DownloadModel.fertig;
-                case Download.FEHLER_BEIM_FERTIGSTELLEN:
-                    return DownloadModel.fehlerBeimFertigstellen;
-                case Download.NICHT_GENUG_PLATZ_FEHLER:
-                    return DownloadModel.keinPlatz;
-                case Download.DATA_WIRD_ERSTELLT:
-                    return DownloadModel.dataWirdErstellt;
-                case Download.SUCHEN_LADEN: {
-                    DownloadSource[] sources = download.getSources();
-                    String result = "";
-                    int uebertragung = 0;
-                    int warteschlange = 0;
-                    int status;
-                    for (int i = 0; i < sources.length; i++) {
-                        status = sources[i].getStatus();
-                        if (status == DownloadSource.UEBERTRAGUNG) {
-                            uebertragung++;
-                            result = DownloadModel.laden;
-                        }
-                        else if (status == DownloadSource.IN_WARTESCHLANGE
-                                 || status == DownloadSource.WARTESCHLANGE_VOLL) {
-                            warteschlange++;
-                        }
-                    }
-                    if (result.length() == 0) {
-                        result = DownloadModel.suchen;
-                    }
-                    return result + " " + (warteschlange + uebertragung) + "/" +
-                        sources.length + " (" + uebertragung + ")";
-                }
-                case Download.FERTIGSTELLEN:
-                    return DownloadModel.fertigstellen;
-                default:
-                    return "";
+   public static String getStatusAsString(Download download)
+   {
+      try
+      {
+         switch(download.getStatus())
+         {
+
+            case Download.PAUSIERT:
+               return DownloadModel.pausiert;
+
+            case Download.ABBRECHEN:
+               return DownloadModel.abbrechen;
+
+            case Download.ABGEGROCHEN:
+               return DownloadModel.abgebrochen;
+
+            case Download.FERTIG:
+               return DownloadModel.fertig;
+
+            case Download.FEHLER_BEIM_FERTIGSTELLEN:
+               return DownloadModel.fehlerBeimFertigstellen;
+
+            case Download.NICHT_GENUG_PLATZ_FEHLER:
+               return DownloadModel.keinPlatz;
+
+            case Download.DATA_WIRD_ERSTELLT:
+               return DownloadModel.dataWirdErstellt;
+
+            case Download.SUCHEN_LADEN:
+            {
+               DownloadSource[] sources       = download.getSources();
+               String           result        = "";
+               int              uebertragung  = 0;
+               int              warteschlange = 0;
+               int              status;
+
+               for(int i = 0; i < sources.length; i++)
+               {
+                  status = sources[i].getStatus();
+                  if(status == DownloadSource.UEBERTRAGUNG)
+                  {
+                     uebertragung++;
+                     result = DownloadModel.laden;
+                  }
+                  else if(status == DownloadSource.IN_WARTESCHLANGE || status == DownloadSource.WARTESCHLANGE_VOLL)
+                  {
+                     warteschlange++;
+                  }
+               }
+
+               if(result.length() == 0)
+               {
+                  result = DownloadModel.suchen;
+               }
+
+               return result + " " + (warteschlange + uebertragung) + "/" + sources.length + " (" + uebertragung + ")";
             }
-        }
-        catch (Exception e) {
-            logger.error(ApplejuiceFassade.ERROR_MESSAGE, e);
-            return "";
-        }
-    }	
+
+            case Download.FERTIGSTELLEN:
+               return DownloadModel.fertigstellen;
+
+            default:
+               return "";
+         }
+      }
+      catch(Exception e)
+      {
+         logger.error(ApplejuiceFassade.ERROR_MESSAGE, e);
+         return "";
+      }
+   }
 }
--- AJClientGUI/src/de/applejuicenet/client/shared/util/UploadCalculator.java
+++ AJClientGUI/src/de/applejuicenet/client/shared/util/UploadCalculator.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2006 TKLSoft.de   All rights reserved.
+ */
+
 package de.applejuicenet.client.shared.util;
 
 import java.awt.Component;
@@ -11,95 +15,133 @@ import de.applejuicenet.client.fassade.entity.Upload;
 import de.applejuicenet.client.fassade.entity.Version;
 import de.applejuicenet.client.shared.IconManager;
 
-public abstract class UploadCalculator {
-	
-	private static JProgressBar progress = new JProgressBar(JProgressBar.HORIZONTAL, 0, 100);
-	private static JProgressBar wholeLoadedProgress = new JProgressBar(JProgressBar.HORIZONTAL, 0, 100);
-	private static JLabel progressbarLabel = new JLabel();
-	private static JLabel versionLabel = new JLabel();
-	private static JLabel wholeLoadedProgressbarLabel = new JLabel();
-	static {
-        progress.setStringPainted(true);
-        progress.setOpaque(false);
-        progressbarLabel = new JLabel();
-        progressbarLabel.setOpaque(true);
-        versionLabel.setOpaque(true);
-        wholeLoadedProgress.setStringPainted(true);
-        wholeLoadedProgress.setOpaque(false);
-        wholeLoadedProgressbarLabel.setOpaque(true);
-	}
+public abstract class UploadCalculator
+{
+   private static JProgressBar progress                    = new JProgressBar(JProgressBar.HORIZONTAL, 0, 100);
+   private static JProgressBar wholeLoadedProgress         = new JProgressBar(JProgressBar.HORIZONTAL, 0, 100);
+   private static JLabel       progressbarLabel            = new JLabel();
+   private static JLabel       versionLabel                = new JLabel();
+   private static JLabel       wholeLoadedProgressbarLabel = new JLabel();
 
-	 public static Component getProgressbarComponent(Upload upload) {
-        if (upload.getStatus() == Upload.AKTIVE_UEBERTRAGUNG) {
-            String prozent = upload.getDownloadPercentAsString();
-            progress.setString(prozent + " %");
-            int pos = prozent.indexOf(',');
-            if (pos != -1) {
-                prozent = prozent.substring(0, pos);
+   static
+   {
+      progress.setStringPainted(true);
+      progress.setOpaque(false);
+      progressbarLabel = new JLabel();
+      progressbarLabel.setOpaque(true);
+      versionLabel.setOpaque(true);
+      wholeLoadedProgress.setStringPainted(true);
+      wholeLoadedProgress.setOpaque(false);
+      wholeLoadedProgressbarLabel.setOpaque(true);
+   }
+
+   public static Component getProgressbarComponent(Upload upload)
+   {
+      if(upload.getStatus() == Upload.AKTIVE_UEBERTRAGUNG)
+      {
+         String prozent = upload.getDownloadPercentAsString();
+
+         progress.setString(prozent + " %");
+         int pos = prozent.indexOf(',');
+
+         if(pos != -1)
+         {
+            prozent = prozent.substring(0, pos);
+         }
+         else
+         {
+            pos = prozent.indexOf('.');
+            if(pos != -1)
+            {
+               prozent = prozent.substring(0, pos);
             }
-            progress.setValue(Integer.parseInt(prozent));
-            return progress;
-        }
-        else {
-            return progressbarLabel;
-        }
-    }
+         }
 
-    public static Component getVersionComponent(Upload upload, JTable table) {
-        versionLabel.setFont(table.getFont());
-        if (upload.getVersion() == null) {
-            versionLabel.setIcon(null);
-            versionLabel.setText("");
-        }
-        else {
-            versionLabel.setIcon(getVersionIcon(upload));
-            versionLabel.setText(upload.getVersion().getVersion());
-        }
-        return versionLabel;
-    }
+         progress.setValue(Integer.parseInt(prozent));
+         return progress;
+      }
+      else
+      {
+         return progressbarLabel;
+      }
+   }
 
-    public static Component getWholeLoadedProgressbarComponent(Upload upload) {
-        if (upload.getLoaded() != -1) {
-            String prozent = upload.getLoadedPercentAsString();
-            wholeLoadedProgress.setString(prozent + " %");
-            int pos = prozent.indexOf(',');
-            if (pos != -1) {
-                prozent = prozent.substring(0, pos);
+   public static Component getVersionComponent(Upload upload, JTable table)
+   {
+      versionLabel.setFont(table.getFont());
+      if(upload.getVersion() == null)
+      {
+         versionLabel.setIcon(null);
+         versionLabel.setText("");
+      }
+      else
+      {
+         versionLabel.setIcon(getVersionIcon(upload));
+         versionLabel.setText(upload.getVersion().getVersion());
+      }
+
+      return versionLabel;
+   }
+
+   public static Component getWholeLoadedProgressbarComponent(Upload upload)
+   {
+      if(upload.getLoaded() != -1)
+      {
+         String prozent = upload.getLoadedPercentAsString();
+
+         wholeLoadedProgress.setString(prozent + " %");
+         int pos = prozent.indexOf(',');
+
+         if(pos != -1)
+         {
+            prozent = prozent.substring(0, pos);
+         }
+         else
+         {
+            pos = prozent.indexOf('.');
+            if(pos != -1)
+            {
+               prozent = prozent.substring(0, pos);
             }
-            wholeLoadedProgress.setValue(Integer.parseInt(prozent));
-            return wholeLoadedProgress;
-        }
-        else {
-            return wholeLoadedProgressbarLabel;
-        }
-    }
+         }
 
-	public static ImageIcon getVersionIcon(Upload upload) {
-        switch (upload.getVersion().getBetriebsSystem()) {
-	        case Version.WIN32: {
-	            return IconManager.getInstance().getIcon("winsymbol");
-	        }
-	        case Version.LINUX: {
-	            return IconManager.getInstance().getIcon("linuxsymbol");
-	        }
-	        case Version.FREEBSD: {
-	            return IconManager.getInstance().getIcon("freebsdsymbol");
-	        }
-	        case Version.MACINTOSH: {
-	            return IconManager.getInstance().getIcon("macsymbol");
-	        }
-	        case Version.SOLARIS: {
-	            return IconManager.getInstance().getIcon("sunossymbol");
-	        }
-	        case Version.NETWARE: {
-	            return IconManager.getInstance().getIcon("netwaresymbol");
-	        }
-	        case Version.OS2: {
-	            return IconManager.getInstance().getIcon("os2symbol");
-	        }
-	        default: {
-	            return IconManager.getInstance().getIcon("unbekanntsymbol");
-	        }
-        }
-    }
+         wholeLoadedProgress.setValue(Integer.parseInt(prozent));
+         return wholeLoadedProgress;
+      }
+      else
+      {
+         return wholeLoadedProgressbarLabel;
+      }
+   }
+
+   public static ImageIcon getVersionIcon(Upload upload)
+   {
+      switch(upload.getVersion().getBetriebsSystem())
+      {
+
+         case Version.WIN32:
+            return IconManager.getInstance().getIcon("winsymbol");
+
+         case Version.LINUX:
+            return IconManager.getInstance().getIcon("linuxsymbol");
+
+         case Version.FREEBSD:
+            return IconManager.getInstance().getIcon("freebsdsymbol");
+
+         case Version.MACINTOSH:
+            return IconManager.getInstance().getIcon("macsymbol");
+
+         case Version.SOLARIS:
+            return IconManager.getInstance().getIcon("sunossymbol");
+
+         case Version.NETWARE:
+            return IconManager.getInstance().getIcon("netwaresymbol");
+
+         case Version.OS2:
+            return IconManager.getInstance().getIcon("os2symbol");
+
+         default:
+            return IconManager.getInstance().getIcon("unbekanntsymbol");
+      }
+   }
 }
