1b429ead0e0c12830f3c29f3418128d7f77850d2
maj0r <maj0r>
2009-01-07 15:35:52 +0000

    *** empty log message ***

--- AJClientGUI/plugin_src/logviewer/src/de/applejuicenet/client/gui/plugins/logviewer/SortedStringListModel.java
+++ AJClientGUI/plugin_src/logviewer/src/de/applejuicenet/client/gui/plugins/logviewer/SortedStringListModel.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2006 TKLSoft.de   All rights reserved.
+ */
+
 package de.applejuicenet.client.gui.plugins.logviewer;
 
 import java.util.HashSet;
@@ -6,32 +10,38 @@ import java.util.TreeSet;
 import javax.swing.ListModel;
 import javax.swing.event.ListDataListener;
 
+public class SortedStringListModel implements ListModel
+{
+   @SuppressWarnings("unchecked")
+   private TreeSet<String>                               data = new TreeSet<String>(new StringComparator());
+   private HashSet<ListDataListener>                     listeners = new HashSet<ListDataListener>();
 
-public class SortedStringListModel implements ListModel{
-    
-    private TreeSet<String> data = new TreeSet<String>(new StringComparator());
-    private HashSet<ListDataListener> listeners = new HashSet<ListDataListener>();
+   public void setData(String[] newData)
+   {
+      data.clear();
+      for(String curValue : newData)
+      {
+         data.add(curValue);
+      }
+   }
 
-    public void setData(String[] newData){
-        data.clear();
-        for (String curValue:  newData){
-            data.add(curValue);
-        }
-    }
-    
-    public int getSize() {
-        return data.size();
-    }
+   public int getSize()
+   {
+      return data.size();
+   }
 
-    public Object getElementAt(int index) {
-        return data.toArray()[index];
-    }
+   public Object getElementAt(int index)
+   {
+      return data.toArray()[index];
+   }
 
-    public void addListDataListener(ListDataListener l) {
-        listeners.add(l);
-    }
+   public void addListDataListener(ListDataListener l)
+   {
+      listeners.add(l);
+   }
 
-    public void removeListDataListener(ListDataListener l) {
-        listeners.remove(l);
-    }
+   public void removeListDataListener(ListDataListener l)
+   {
+      listeners.remove(l);
+   }
 }
