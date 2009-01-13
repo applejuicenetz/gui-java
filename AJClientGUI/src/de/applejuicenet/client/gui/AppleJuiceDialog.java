76a880035f2da89d37ac0610816447a85d915a77
maj0r <maj0r>
2009-01-13 15:36:24 +0000

    *** empty log message ***

--- AJClientGUI/src/de/applejuicenet/client/gui/AppleJuiceDialog.java
+++ AJClientGUI/src/de/applejuicenet/client/gui/AppleJuiceDialog.java
@@ -133,7 +133,7 @@ public class AppleJuiceDialog extends TKLFrame implements LanguageListener, Data
 {
 
    //CVS-Beispiel 0.60.0-1-CVS
-   public static final String       GUI_VERSION              = "0.70.4-1-CVS";
+   public static final String       GUI_VERSION              = "0.70.4";
    private static Logger            logger                   = Logger.getLogger(AppleJuiceDialog.class);
    private static Map<String, Skin> themes                   = null;
    public static boolean            rewriteProperties        = false;
