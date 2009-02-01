e35c8fa10e1a33cfaa11142f6ac7b015d2d6506f
maj0r <maj0r>
2009-02-01 16:04:13 +0000

    *** empty log message ***

--- ajtray/src/de/applejuicenet/client/gui/tray/TrayLoader.java
+++ ajtray/src/de/applejuicenet/client/gui/tray/TrayLoader.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2006 TKLSoft.de   All rights reserved.
  */
-
 package de.applejuicenet.client.gui.tray;
 
 import java.awt.AWTException;
 import java.awt.Image;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import java.awt.TrayIcon.MessageType;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -19,15 +19,16 @@ import de.applejuicenet.client.gui.AppleJuiceDialog;
 
 public class TrayLoader implements TrayIF
 {
-   private String zeigen;
-   private String verstecken;
+   private String   zeigen;
+   private String   verstecken;
+   private TrayIcon trayIcon = null;
 
    public boolean makeTray(Image image, String title, final AppleJuiceDialog dialog, final JMenuItem popupShowHideMenuItem,
                            final Icon zeigenIcon, final Icon versteckenIcon, final JPopupMenu popup)
    {
-      SystemTray     tray = SystemTray.getSystemTray();
+      SystemTray tray = SystemTray.getSystemTray();
 
-      final TrayIcon trayIcon = new TrayIcon(image, title, null);
+      trayIcon = new TrayIcon(image, title, null);
 
       trayIcon.setImageAutoSize(true);
       trayIcon.addMouseListener(new MouseAdapter()
@@ -97,5 +98,15 @@ public class TrayLoader implements TrayIF
    public void setTextVerstecken(String verstecken)
    {
       this.verstecken = verstecken;
+   }
+
+   public void showBallon(String caption, String message)
+   {
+      if(null == trayIcon || (null == caption && null == message))
+      {
+         return;
+      }
+
+      trayIcon.displayMessage(caption, message, MessageType.INFO);
    }
 }
