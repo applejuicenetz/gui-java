5d8e090f7a286045d78456b0113d6a603ef37f4e
red171 <red171@applejuicenet.cc>
2022-02-02 09:33:46 +0100

    load shipped plugin on all platforms

--- modules/AJCoreGUI/src/main/java/de/applejuicenet/client/gui/plugincontrol/PluginFactory.java
+++ modules/AJCoreGUI/src/main/java/de/applejuicenet/client/gui/plugincontrol/PluginFactory.java
@@ -6,9 +6,7 @@ package de.applejuicenet.client.gui.plugincontrol;
 
 import java.io.File;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -18,139 +16,117 @@ import org.apache.log4j.Logger;
 import de.applejuicenet.client.gui.plugins.PluginConnector;
 import de.applejuicenet.client.shared.PluginJarClassLoader;
 
-public abstract class PluginFactory
-{
-   private static Set<PluginConnector> plugins = null;
-   private static final Logger               logger = Logger.getLogger(PluginFactory.class);
+public abstract class PluginFactory {
+    private static Set<PluginConnector> plugins = null;
+    private static final Logger logger = Logger.getLogger(PluginFactory.class);
 
-   public static Set<PluginConnector> getPlugins()
-   {
-      if(null == plugins)
-      {
-         boolean isDebugPlugins = System.getProperty("Plugins") != null;
+    public static Set<PluginConnector> getPlugins() {
+        if (null == plugins) {
+            boolean isDebugPlugins = System.getProperty("Plugins") != null;
 
-         if(isDebugPlugins)
-         {
-            plugins = loadPluginsFromClasspath();
-            return plugins;
-         }
-
-         plugins = new HashSet<PluginConnector>();
-
-         String path;
-
-         if(System.getProperty("os.name").toLowerCase().indexOf("windows") == -1)
-         {
-            path = System.getProperty("user.home") + File.separator + "appleJuice" + File.separator + "gui" + File.separator +
-                   "plugins" + File.separator;
-         }
-         else
-         {
-            path = System.getProperty("user.dir") + File.separator + "plugins" + File.separator;
-         }
-
-         File pluginPath = new File(path);
-
-         String[]             tempListe = pluginPath.list();
-         PluginJarClassLoader jarLoader = null;
-
-         for(int i = 0; i < Objects.requireNonNull(tempListe).length; i++)
-         {
-            if(tempListe[i].toLowerCase().endsWith(".jar"))
-            {
-               try
-               {
-                  File pluginFile = new File(path + tempListe[i]);
-
-                  if(pluginFile.isFile())
-                  {
-                     ZipFile  jf    = new ZipFile(pluginFile);
-                     ZipEntry entry = jf.getEntry("plugin.properties");
-
-                     if(entry == null)
-                     {
-                        continue;
-                     }
-                  }
-                  else
-                  {
-                     continue;
-                  }
-
-                  jarLoader = new PluginJarClassLoader();
-                  PluginConnector aPlugin = jarLoader.getPlugin(new File(path + tempListe[i]));
-
-                  if(aPlugin != null)
-                  {
-                     plugins.add(aPlugin);
-                     String nachricht = "Plugin " + aPlugin.getTitle() + " geladen...";
-
-                     if(logger.isEnabledFor(Level.INFO))
-                     {
-                        logger.info(nachricht);
-                     }
-                  }
-               }
-               catch(Exception e)
-               {
-                  //Von einem Plugin lassen wir uns nicht beirren! ;-)
-                  logger.error("Ein Plugin konnte nicht instanziert werden", e);
-                  continue;
-               }
+            if (isDebugPlugins) {
+                plugins = loadPluginsFromClasspath();
+                return plugins;
             }
-         }
-      }
 
-      return plugins;
-   }
+            plugins = new HashSet<>();
 
-   private static Set<PluginConnector> loadPluginsFromClasspath()
-   {
-      Set<PluginConnector> thePlugins = new HashSet<PluginConnector>();
-      String[]             which = new String[] { //"de.applejuicenet.client.gui.plugins.jabber.JabberTestLoader",//
-//              "de.applejuicenet.client.gui.plugins.versionchecker.VersioncheckerTestLoader"
+            Map<String, File> tempListe = new HashMap<>();
+
+            String shippedPath = System.getProperty("user.dir") + File.separator + "plugins" + File.separator;
+            String userPath = System.getProperty("user.home") + File.separator + "appleJuice" + File.separator + "gui" + File.separator + "plugins" + File.separator;
+
+            File userPathFile = new File(userPath);
+            File shippedPathFile = new File(shippedPath);
+
+            File[] userPathFiles = userPathFile.listFiles();
+            if (userPathFiles != null) {
+                for (File pathFile : userPathFiles) {
+                    if (pathFile.isFile() && pathFile.getName().endsWith(".jar")) {
+                        tempListe.put(pathFile.getName(), pathFile);
+                    }
+                }
+            }
+
+            File[] shippedPathFiles = shippedPathFile.listFiles();
+            if (shippedPathFiles != null) {
+                for (File pathFile : shippedPathFiles) {
+                    if (pathFile.isFile() && pathFile.getName().endsWith(".jar")) {
+                        if (!tempListe.containsKey(pathFile.getName())) {
+                            tempListe.put(pathFile.getName(), pathFile);
+                        }
+                    }
+                }
+            }
+
+            PluginJarClassLoader jarLoader;
+
+            for (Map.Entry<String, File> pluginFile : tempListe.entrySet()) {
+                try {
+                    ZipFile jf = new ZipFile(pluginFile.getValue());
+                    ZipEntry entry = jf.getEntry("plugin.properties");
+
+                    if (entry == null) {
+                        continue;
+                    }
+
+                    jarLoader = new PluginJarClassLoader();
+                    PluginConnector aPlugin = jarLoader.getPlugin(pluginFile.getValue());
+
+                    if (aPlugin != null) {
+                        plugins.add(aPlugin);
+                        String nachricht = "Plugin " + aPlugin.getTitle() + " geladen...";
+
+                        if (logger.isEnabledFor(Level.INFO)) {
+                            logger.info(nachricht);
+                        }
+                    }
+                } catch (Exception e) {
+                    //Von einem Plugin lassen wir uns nicht beirren! ;-)
+                    logger.error("Ein Plugin konnte nicht instanziert werden", e);
+                }
+            }
+        }
+
+        return plugins;
+    }
+
+    private static Set<PluginConnector> loadPluginsFromClasspath() {
+        Set<PluginConnector> thePlugins = new HashSet<PluginConnector>();
+        String[] which = new String[]{ //"de.applejuicenet.client.gui.plugins.jabber.JabberTestLoader",//
 //                                                                       "de.applejuicenet.client.gui.plugins.serverwatcher.ServerWatcherTestLoader"
 //                                                                       "de.applejuicenet.client.gui.plugins.logviewer.LogViewerTestLoader"
-              "de.applejuicenet.client.gui.plugins.speedgraph.SpeedGraphPluginTestLoader"
-//         "de.applejuicenet.client.gui.plugins.ircplugin.IrcPluginTestLoader"
-              };
+                "de.applejuicenet.client.gui.plugins.speedgraph.SpeedGraphPluginTestLoader"
+        };
 
-      for(String curWhich : which)
-      {
-         PluginConnector plugin = loadPlugin(curWhich);
+        for (String curWhich : which) {
+            PluginConnector plugin = loadPlugin(curWhich);
 
-         if(null != plugin)
-         {
-            thePlugins.add(plugin);
-            String nachricht = "Plugin " + plugin.getTitle() + " geladen...";
+            if (null != plugin) {
+                thePlugins.add(plugin);
+                String nachricht = "Plugin " + plugin.getTitle() + " geladen...";
 
-            if(logger.isEnabledFor(Level.INFO))
-            {
-               logger.info(nachricht);
+                if (logger.isEnabledFor(Level.INFO)) {
+                    logger.info(nachricht);
+                }
             }
-         }
-      }
+        }
 
-      return thePlugins;
-   }
+        return thePlugins;
+    }
 
-   @SuppressWarnings("unchecked")
-   private static PluginConnector loadPlugin(String which)
-   {
-      try
-      {
-         Class           pluginClass     = Class.forName(which);
-         TestLoader      testLoader      = (TestLoader) pluginClass.newInstance();
-         PluginConnector pluginConnector = testLoader.getPlugin();
+    private static PluginConnector loadPlugin(String which) {
+        try {
+            Class pluginClass = Class.forName(which);
+            TestLoader testLoader = (TestLoader) pluginClass.newInstance();
+            PluginConnector pluginConnector = testLoader.getPlugin();
 
-         return pluginConnector;
-      }
-      catch(Throwable e)
-      {
-         //Von einem Plugin lassen wir uns nicht beirren! ;-)
-         logger.error("Ein Plugin konnte nicht instanziert werden", e);
-      }
+            return pluginConnector;
+        } catch (Throwable e) {
+            //Von einem Plugin lassen wir uns nicht beirren! ;-)
+            logger.error("Ein Plugin konnte nicht instanziert werden", e);
+        }
 
-      return null;
-   }
+        return null;
+    }
 }
